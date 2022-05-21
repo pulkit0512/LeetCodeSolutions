2bc9d9b5922a2baeeb117a1235422832f6ba1336
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-21 17:37:09 +0530

    Time: 116 ms (15.04%), Space: 124.5 MB (60.15%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,30 +1,76 @@
 class LRUCache {
-    Map<Integer, Integer> cache;
+    class DLLNode{
+        int key, val;
+        DLLNode next, prev;
+        DLLNode(int key, int val){
+            this.key = key;
+            this.val = val;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    
+    private void addNode(DLLNode node){
+        node.prev = head;
+        node.next = head.next;
+        head.next = node;
+        node.next.prev = node;
+    }
+    
+    private void removeNode(DLLNode node){
+        DLLNode prev = node.prev;
+        DLLNode next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+    
+    private void moveNodeToTop(DLLNode node){
+        removeNode(node);
+        addNode(node);
+    }
+    
+    private int removeLast(){
+        DLLNode prev = tail.prev;
+        removeNode(prev);
+        return prev.key;
+    }
+    
+    Map<Integer, DLLNode> cache;
     int capacity;
+    DLLNode head, tail;
 
     public LRUCache(int capacity) {
+        this.cache = new HashMap<>();
         this.capacity = capacity;
-        cache = new LinkedHashMap<>();
+        head = new DLLNode(-1, -1);
+        tail = new DLLNode(-1, -1);
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        if(!cache.containsKey(key)){
+        DLLNode node = cache.get(key);
+        if(node==null){
             return -1;
         }
-        int val = cache.get(key);
-        cache.remove(key);
-        cache.put(key, val);
-        return val;
+        moveNodeToTop(node);
+        return node.val;
     }
     
     public void put(int key, int value) {
-        if(cache.containsKey(key)){
-            cache.remove(key);
-        }else if(cache.size()==capacity){
-            int firstKey = cache.entrySet().iterator().next().getKey();
-            cache.remove(firstKey);
+        DLLNode node = cache.get(key);
+        if(node == null){
+            DLLNode newNode = new DLLNode(key, value);
+            addNode(newNode);
+            cache.put(key, newNode);
+            if(cache.size()>capacity){
+                int lastKey = removeLast();
+                cache.remove(lastKey);
+            }
+        }else{
+            node.val = value;
+            moveNodeToTop(node);
         }
-        cache.put(key, value);
     }
 }
 
