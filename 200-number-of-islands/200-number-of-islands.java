fe63b705bfb828447ea5046c35eac805ce18959c
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-21 18:41:30 +0530

    Time: 8 ms (25.68%), Space: 56.7 MB (59.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -6,21 +6,45 @@ class Solution {
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
                     ans++;
-                    dfs(i, j, grid);
+                    //dfs(i, j, grid);
+                    bfs(i, j, grid);
                 }
             }
         }
         return ans;
+    }
+    private void bfs(int row, int col, char[][] grid){
+        Queue<Integer> que = new LinkedList<>();
+        int nr = grid.length;
+        int nc = grid[0].length;
+        que.add(row*nc + col);
+        grid[row][col] = '0';
+        while(!que.isEmpty()){
+            int val = que.poll();
+            int r = val/nc;
+            int c = val%nc;
+            for(int i=0;i<4;i++){
+                int newRow = r + dir[i][0];
+                int newCol = c + dir[i][1];
+                if(isValid(newRow, newCol, grid)){
+                    que.add(newRow*nc + newCol);
+                    grid[newRow][newCol] = '0';
+                }
+            }
+        }
     }
     private void dfs(int row, int col, char[][] grid){
         grid[row][col] = '0';
         for(int i=0;i<4;i++){
             int newRow = row + dir[i][0];
             int newCol = col + dir[i][1];
-            if(newRow>=0 && newRow<grid.length && newCol>=0 
-               && newCol<grid[0].length && grid[newRow][newCol]=='1'){
+            if(isValid(newRow, newCol, grid)){
                 dfs(newRow, newCol, grid);
             }
         }
+    }
+    private boolean isValid(int newRow, int newCol, char[][] grid){
+        return newRow>=0 && newRow<grid.length && newCol>=0 
+               && newCol<grid[0].length && grid[newRow][newCol]=='1';
     }
 }
