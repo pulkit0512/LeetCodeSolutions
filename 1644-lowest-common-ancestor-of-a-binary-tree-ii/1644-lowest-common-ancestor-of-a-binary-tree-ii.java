bb77546013801136eba4c5e8a8f8efda263ccb54
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-22 03:07:15 +0530

    Time: 90 ms (5.22%), Space: 72.5 MB (22.09%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.java
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.java
@@ -11,14 +11,22 @@ class Solution {
     private static final int BOTH_DONE = 0;
     private static final int LEFT_DONE = 1;
     private static final int BOTH_PENDING = 2;
+    class pair{
+        TreeNode key;
+        int val;
+        pair(TreeNode a, int b){
+            key = a;
+            val = b;
+        }
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Stack<Pair<TreeNode, Integer>> st = new Stack<>();
+        Stack<pair> st = new Stack<>();
         TreeNode LCA = null, childNode = null;
         boolean oneNodeFound = false;
-        st.push(new Pair(root, BOTH_PENDING));
+        st.push(new pair(root, BOTH_PENDING));
         while(!st.isEmpty()){
-            TreeNode parent = st.peek().getKey();
-            int parentState = st.peek().getValue();
+            TreeNode parent = st.peek().key;
+            int parentState = st.peek().val;
             if(parentState!=BOTH_DONE){
                 if(parentState==BOTH_PENDING){
                     if(parent.val==p.val || parent.val==q.val){
@@ -34,14 +42,14 @@ class Solution {
                     childNode = parent.right;
                 }
                 st.pop();
-                st.push(new Pair(parent, parentState-1));
+                st.push(new pair(parent, parentState-1));
                 if(childNode!=null){
-                    st.push(new Pair(childNode, BOTH_PENDING));
+                    st.push(new pair(childNode, BOTH_PENDING));
                 }
             }else{
-                if(LCA == st.pop().getKey() && oneNodeFound){
+                if(LCA == st.pop().key && oneNodeFound){
                     if(!st.isEmpty()){
-                        LCA = st.peek().getKey();
+                        LCA = st.peek().key;
                     }
                 }
             }
