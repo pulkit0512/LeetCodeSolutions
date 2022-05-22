bca6c82c3eee8634ca7cab00f63dd8748bb16eff
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-22 15:46:33 +0530

    Time: 5 ms (74.96%), Space: 42.5 MB (39.02%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,5 +1,42 @@
 class Solution {
     public int countSubstrings(String s) {
+        //return countSubstringsN2Space(s);
+        return countSubstringsConstantSpace(s);
+    }
+    
+    private int countSubstringsConstantSpace(String s){
+        int len = s.length();
+        int ans = len;
+        for(int i=1;i<len;i++){
+            //find Even Palindrome
+            int x = i-1, y = i;
+            while(x>=0 && y<len){
+                if(s.charAt(x)==s.charAt(y)){
+                    x--;
+                    y++;
+                    ans++;
+                }else{
+                    break;
+                }
+            }
+            
+            //find odd Palindrome
+            x = i-1;
+            y = i+1;
+            while(x>=0 && y<len){
+                if(s.charAt(x)==s.charAt(y)){
+                    x--;
+                    y++;
+                    ans++;
+                }else{
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    private int countSubstringsN2Space(String s){
         int len = s.length();
         boolean dp[][] = new boolean[len][len];
         int ans = 0;
