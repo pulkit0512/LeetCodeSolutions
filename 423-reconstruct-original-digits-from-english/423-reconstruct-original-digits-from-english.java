af2a3dd14c8e699afb32457e6d709b0ef0f5b55b
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-23 01:57:15 +0530

    Time: 40 ms (15.98%), Space: 53 MB (19.63%) - LeetHub

--- 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
@@ -9,16 +9,16 @@ class Solution {
         //System.out.println(freqMap);
         StringBuilder sb = new StringBuilder();
         int cnt[] = new int[10];
-        cnt[0] = check(freqMap, 'z', "zero", 4);
-        cnt[2] = check(freqMap, 'w', "two", 3);
-        cnt[4] = check(freqMap, 'u', "four", 4);
-        cnt[1] = check(freqMap, 'o', "one", 3);
-        cnt[5] = check(freqMap, 'f', "five", 4);
-        cnt[6] = check(freqMap, 'x', "six", 3);
-        cnt[7] = check(freqMap, 's', "seven", 5);
-        cnt[8] = check(freqMap, 'g', "eight", 5);
-        cnt[9] = check(freqMap, 'i', "nine", 4);
-        cnt[3] = check(freqMap, 't', "three", 5);
+        cnt[0] = freqMap.getOrDefault('z', 0);
+        cnt[2] = freqMap.getOrDefault('w', 0);
+        cnt[4] = freqMap.getOrDefault('u', 0);
+        cnt[6] = freqMap.getOrDefault('x', 0);
+        cnt[8] = freqMap.getOrDefault('g', 0);
+        cnt[5] = freqMap.getOrDefault('f', 0) - cnt[4];
+        cnt[7] = freqMap.getOrDefault('s', 0) - cnt[6];
+        cnt[3] = freqMap.getOrDefault('h', 0) - cnt[8];
+        cnt[9] = freqMap.getOrDefault('i', 0) - cnt[5] - cnt[6] - cnt[8];
+        cnt[1] = freqMap.getOrDefault('o', 0) - cnt[0] - cnt[2] - cnt[4];
         
         for(int i=0;i<=9;i++){
             for(int j=0;j<cnt[i];j++){
@@ -26,22 +26,5 @@ class Solution {
             }
         }
         return sb.toString();
-    }
-    
-    private int check(Map<Character, Integer> freqMap, char ch, String s, int len){
-        if(freqMap.isEmpty() || !freqMap.containsKey(ch)){
-            return 0;
-        }
-        int val = freqMap.get(ch);
-        for(int i=0;i<len;i++){
-            char temp = s.charAt(i);
-            int k = freqMap.get(temp);
-            if(k==val){
-                freqMap.remove(temp);
-            }else{
-                freqMap.put(temp, k-val);
-            }
-        }
-        return val;
     }
 }
