77aa8b5e3dc64a2872715aa87f2244f24692bd42
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-23 02:01:46 +0530

    Time: 11 ms (46.06%), Space: 46.6 MB (62.13%) - LeetHub

--- 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.java
@@ -2,23 +2,24 @@ class Solution {
     public String originalDigits(String s) {
         Map<Character, Integer> freqMap = new HashMap<>();
         int len = s.length();
+        int arr[] = new int[26];
         for(int i=0;i<len;i++){
             char ch = s.charAt(i);
-            freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
+            arr[ch-'a']++;
         }
         //System.out.println(freqMap);
         StringBuilder sb = new StringBuilder();
         int cnt[] = new int[10];
-        cnt[0] = freqMap.getOrDefault('z', 0);
-        cnt[2] = freqMap.getOrDefault('w', 0);
-        cnt[4] = freqMap.getOrDefault('u', 0);
-        cnt[6] = freqMap.getOrDefault('x', 0);
-        cnt[8] = freqMap.getOrDefault('g', 0);
-        cnt[5] = freqMap.getOrDefault('f', 0) - cnt[4];
-        cnt[7] = freqMap.getOrDefault('s', 0) - cnt[6];
-        cnt[3] = freqMap.getOrDefault('h', 0) - cnt[8];
-        cnt[9] = freqMap.getOrDefault('i', 0) - cnt[5] - cnt[6] - cnt[8];
-        cnt[1] = freqMap.getOrDefault('o', 0) - cnt[0] - cnt[2] - cnt[4];
+        cnt[0] = arr['z'-'a'];
+        cnt[2] = arr['w'-'a'];
+        cnt[4] = arr['u'-'a'];
+        cnt[6] = arr['x'-'a'];
+        cnt[8] = arr['g'-'a'];
+        cnt[5] = arr['f'-'a'] - cnt[4];
+        cnt[7] = arr['s'-'a'] - cnt[6];
+        cnt[3] = arr['h'-'a'] - cnt[8];
+        cnt[9] = arr['i'-'a'] - cnt[5] - cnt[6] - cnt[8];
+        cnt[1] = arr['o'-'a'] - cnt[0] - cnt[2] - cnt[4];
         
         for(int i=0;i<=9;i++){
             for(int j=0;j<cnt[i];j++){
