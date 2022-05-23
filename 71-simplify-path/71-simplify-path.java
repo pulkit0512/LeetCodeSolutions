d1e622030de79e74931a7e4e51feff779246f2e5
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-24 02:06:22 +0530

    Time: 2 ms (99.98%), Space: 42.2 MB (91.94%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,65 @@
+class Solution {
+    public String simplifyPath(String path) {
+        StringBuilder sb = new StringBuilder();
+        int len = path.length();
+        int dot = 0, chars = 0;
+        for(int i=0;i<len;i++){
+            char ch = path.charAt(i);
+            if(ch=='.'){
+                if(sb.length()==0){
+                    continue;
+                }else{
+                    sb.append(ch);
+                    if(chars==0){
+                        dot++;
+                    }
+                }
+            }else if(ch=='/'){
+                if(sb.length()==0){
+                    sb.append(ch);
+                }else if(sb.charAt(sb.length()-1)=='/'){
+                    continue;
+                }else if(dot==1){
+                    sb.deleteCharAt(sb.length()-1);
+                    dot = 0;
+                }else if(dot==2){
+                    sb.deleteCharAt(sb.length()-1);
+                    sb.deleteCharAt(sb.length()-1);
+                    dot = 0;
+                    if(sb.length()>1){
+                        sb.deleteCharAt(sb.length()-1);
+                        while(sb.charAt(sb.length()-1)!='/'){
+                            sb.deleteCharAt(sb.length()-1);
+                        }
+                    }
+                }else{
+                    dot = 0;
+                    chars = 0;
+                    sb.append(ch);
+                }
+            }else{
+                sb.append(ch);
+                chars++;
+                dot = 0;
+            }
+        }
+        if(dot==1){
+            sb.deleteCharAt(sb.length()-1);
+            dot = 0;
+        }else if(dot==2){
+            sb.deleteCharAt(sb.length()-1);
+            sb.deleteCharAt(sb.length()-1);
+            dot = 0;
+            if(sb.length()>1){
+                sb.deleteCharAt(sb.length()-1);
+                while(sb.charAt(sb.length()-1)!='/'){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+        }
+        if(sb.length()>1 && sb.charAt(sb.length()-1)=='/'){
+            sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString();
+    }
+}
