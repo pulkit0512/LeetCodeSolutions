da9f995b868fc28100d4516b555271073e3dda5a
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-25 18:30:34 +0530

    Time: 9 ms (75.15%), Space: 43.9 MB (66.90%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,5 +1,44 @@
 class Solution {
     public int calculate(String s) {
+        //return calculateUsingTwoStacks(s);
+        return calculateUsingOneStack(s);
+    }
+    private int calculateUsingOneStack(String s) {
+        s = "("+s+")";
+        int len = s.length();
+        int result = 0, sign = 1;
+        int current = 0;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }else if(ch=='+'){
+                result += current*sign;
+                sign = 1;
+                current = 0;
+            }else if(ch=='-'){
+                result += current*sign;
+                sign = -1;
+                current = 0;
+            }else if(ch ==' '){
+                continue;
+            }else if(Character.isDigit(ch)){
+                current = current*10 + (ch-'0');
+            }else{
+                result += current*sign;
+                result *= st.pop();
+                result += st.pop();
+                current = 0;
+            }
+        }
+        return result;
+    }
+    
+    private int calculateUsingTwoStacks(String s) {
         s = "("+s+")";
         int len = s.length();
         Stack<Integer> values = new Stack<>();
