1c5673d286c9b7a708bdc39f9d5e018948c3d40d
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-25 11:58:50 +0530

    Time: 19 ms (54.41%), Space: 45.4 MB (25.21%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -2,63 +2,48 @@ class Solution {
     public int calculate(String s) {
         int len = s.length();
         Stack<Integer> values = new Stack<>();
-        char opr = ' ';
-        for(int i=0;i<len;){
+        char opr = '+';
+        int val = 0;
+        for(int i=0;i<len;i++){
             char ch = s.charAt(i);
             if(ch==' '){
-                i++;
                 continue;
             }
             if(Character.isDigit(ch)){
-                int val = 0;
                 while(i<len && Character.isDigit(s.charAt(i))){
                     val = val*10 + s.charAt(i)-'0';
                     i++;
                 }
-                values.push(val);
+                i--;
             }else{
-                if(ch=='*' || ch=='/'){
-                    int val1 = values.pop();
-                    i++;
-                    while(s.charAt(i)==' '){
-                        i++;
-                    }
-                    int val2 = 0;
-                    while(i<len && Character.isDigit(s.charAt(i))){
-                        val2 = val2*10 + s.charAt(i)-'0';
-                        i++;
-                    }
-                    if(ch=='*'){
-                        values.push(val1*val2);
-                    }else{
-                        values.push(val1/val2);
-                    }
-                }else if(ch=='+' || ch=='-'){
-                    if(opr==' '){
-                        opr = ch;
-                    }else{
-                        int val2 = values.pop();
-                        int val1 = values.pop();
-                        if(opr=='+'){
-                            values.push(val1+val2);
-                        }else{
-                            values.push(val1-val2);
-                        }
-                        opr = ch;
-                    }
-                    i++;
+                if(opr == '+'){
+                    values.push(val);
+                }else if(opr == '-'){
+                    values.push(-val);
+                }else if(opr == '*'){
+                    values.push(values.pop()*val);
+                }else{
+                    values.push(values.pop()/val);
                 }
+                opr = ch;
+                val = 0;
             }
         }
-        if(opr!=' '){
-            int val2 = values.pop();
-            int val1 = values.pop();
-            if(opr=='+'){
-                values.push(val1+val2);
+        if(val!=0){
+            if(opr == '+'){
+                values.push(val);
+            }else if(opr == '-'){
+                values.push(-val);
+            }else if(opr == '*'){
+                values.push(values.pop()*val);
             }else{
-                values.push(val1-val2);
+                values.push(values.pop()/val);
             }
         }
-        return values.pop();
+        int result = 0;
+        while(!values.isEmpty()){
+            result += values.pop();
+        }
+        return result;
     }
 }
