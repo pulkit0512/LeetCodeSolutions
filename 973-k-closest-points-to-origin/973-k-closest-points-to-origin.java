831f1a60136eb93d72864f0cad71fe282be3e3c5
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-27 00:24:09 +0530

    Time: 38 ms (64.25%), Space: 116.8 MB (48.16%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[2]-a[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (b[0]*b[0] + b[1]*b[1])-(a[0]*a[0] + a[1]*a[1]));
         for(int i=0;i<k;i++){
-            int x = points[i][0]*points[i][0];
-            int y = points[i][1]*points[i][1];
-            pq.add(new int[]{points[i][0], points[i][1], x+y});
+            pq.add(points[i]);
         }
         for(int i=k;i<points.length;i++){
             int x = points[i][0]*points[i][0];
             int y = points[i][1]*points[i][1];
-            if(pq.peek()[2]>x+y){
+            int x1 = pq.peek()[0]*pq.peek()[0];
+            int y1 = pq.peek()[1]*pq.peek()[1];
+            if(x1+y1>x+y){
                 pq.poll();
-                pq.add(new int[]{points[i][0], points[i][1], x+y});
+                pq.add(points[i]);
             }
         }
         int ans[][] = new int[pq.size()][2];
         int idx = 0;
         while(!pq.isEmpty()){
-            ans[idx][0] = pq.peek()[0];
-            ans[idx][1] = pq.peek()[1];
-            pq.poll();
-            idx++;
+            ans[idx++] = pq.poll();
         }
         return ans;
     }
