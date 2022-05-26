200b585a2a265d77d03c0854a0b3cc098cac4510
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-26 21:53:11 +0530

    Time: 227 ms (14.43%), Space: 104 MB (27.82%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,31 +1,46 @@
 class UndergroundSystem {
 
-    Map<Integer, Pair<String, Integer>> custMap;
-    Map<String, Pair<Integer, Integer>> timeMap;
+    class SystemDetails {
+        String station;
+        int time, count;
+        
+        public SystemDetails(int time, int count) {
+            this.time = time;
+            this.count = count;
+        }
+        
+        public SystemDetails(String station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+    }
+    Map<Integer, SystemDetails> custMap;
+    Map<String, SystemDetails> timeMap;
     public UndergroundSystem() {
         custMap = new HashMap<>();
         timeMap = new HashMap<>();
     }
     
     public void checkIn(int id, String stationName, int t) {
-        custMap.put(id, new Pair(stationName, t));
+        custMap.put(id, new SystemDetails(stationName, t));
     }
     
     public void checkOut(int id, String stationName, int t) {
-        Pair<String, Integer> checkIn = custMap.get(id);
-        String stations = checkIn.getKey()+"_"+stationName;
-        int time = t - checkIn.getValue();
+        SystemDetails checkIn = custMap.get(id);
+        String stations = checkIn.station+"_"+stationName;
+        int time = t - checkIn.time;
         if(!timeMap.containsKey(stations)){
-            timeMap.put(stations, new Pair(time, 1));
+            timeMap.put(stations, new SystemDetails(time, 1));
         }else{
-            Pair<Integer, Integer> val = timeMap.get(stations);
-            timeMap.put(stations, new Pair(val.getKey() + time, val.getValue() + 1));
+            SystemDetails val = timeMap.get(stations);
+            val.time += time;
+            val.count += 1;
         }
     }
     
     public double getAverageTime(String startStation, String endStation) {
-        Pair<Integer, Integer> times = timeMap.get(startStation+"_"+endStation);
-        return (1.0 * times.getKey())/times.getValue();
+        SystemDetails details = timeMap.get(startStation+"_"+endStation);
+        return (1.0 * details.time)/details.count;
     }
 }
 
