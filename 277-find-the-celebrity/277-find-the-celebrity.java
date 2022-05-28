d823966e479270f8dd51be41a1b8878371c9af89
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-28 20:29:26 +0530

    Time: 19 ms (65.64%), Space: 54.7 MB (12.91%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -3,6 +3,25 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
+        //return findCelebrityN2(n);
+        
+        return findCelebrityN(n);
+    }
+    
+    private int findCelebrityN(int n) {
+        int celeb = 0;
+        for(int i=1;i<n;i++){
+            if(knows(celeb, i)){
+                celeb = i;
+            }
+        }
+        if(isCeleb(celeb, n)){
+            return celeb;
+        }
+        return -1;
+    }
+    
+    private int findCelebrityN2(int n) {
         for(int i=0;i<n;i++){
             if(isCeleb(i, n)){
                 return i;
