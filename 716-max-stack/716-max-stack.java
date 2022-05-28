d2236ba3a42a0671d446b887f1a60209c7b53ddf
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-29 02:13:49 +0530

    Time: 20 ms (82.71%), Space: 54.4 MB (52.47%) - LeetHub

--- 716-max-stack/716-max-stack.java
+++ 716-max-stack/716-max-stack.java
@@ -1,6 +1,93 @@
 class MaxStack {
+    
+    class Node {
+        int val;
+        Node next, prev;
+        
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+    
+    class DLL {
+        Node head, tail;
+        
+        public DLL() {
+            head = new Node(0);
+            tail = new Node(0);
+            
+            head.next = tail;
+            tail.prev = head;
+        }
+        
+        public Node add(int x) {
+            Node node = new Node(x);
+            node.next = tail;
+            node.prev = tail.prev;
+            tail.prev.next = node;
+            tail.prev = node;
+            return node;
+        }
+        
+        public int peek() {
+            return tail.prev.val;
+        }
+        
+        public int pop() {
+            int val = tail.prev.val;
+            delete(tail.prev);
+            return val;
+        }
+        
+        public void delete(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
+    
+    DLL dll;
+    TreeMap<Integer, List<Node>> map;
+    public MaxStack() {
+        dll = new DLL();
+        map = new TreeMap<>();
+    }
+    
+    public void push(int x) {
+        Node node = dll.add(x);
+        if(!map.containsKey(x)){
+            map.put(x, new ArrayList<>());
+        }
+        map.get(x).add(node);
+    }
+    
+    public int pop() {
+        int val = dll.pop();
+        map.get(val).remove(map.get(val).size()-1);
+        if(map.get(val).isEmpty()){
+            map.remove(val);
+        }
+        return val;
+    }
+    
+    public int top() {
+        return dll.peek();
+    }
+    
+    public int peekMax() {
+        return map.lastKey();
+    }
+    
+    public int popMax() {
+        int val = peekMax();
+        Node node = map.get(val).remove(map.get(val).size() - 1);
+        dll.delete(node);
+        if(map.get(val).isEmpty()){
+            map.remove(val);
+        }
+        return val;
+    }
 
-    Stack<Integer> normal, max;
+    /*Stack<Integer> normal, max;
     public MaxStack() {
         normal = new Stack<>();
         max = new Stack<>();
@@ -39,7 +126,7 @@ class MaxStack {
             push(temp.pop());
         }
         return val;
-    }
+    }*/
 }
 
 /**
