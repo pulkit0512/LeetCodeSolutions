c936ae0c575ca614664854e185d6b6209f5a6202
Pulkit Arora <arorapulkit2@gmail.com>
2022-05-31 22:25:48 +0530

    Time: 18 ms (92.71%), Space: 61.7 MB (63.02%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,5 +1,29 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
+        //return hasAllCodesUsingSet(s, k);
+        return hasAllCodesUsingRollingHash(s, k);
+    }
+    
+    private boolean hasAllCodesUsingRollingHash(String s, int k) {
+        int total = (1<<k);
+        boolean[] hash = new boolean[total];
+        int hashVal = 0;
+        int all1 = total-1;
+        int len = s.length();
+        for(int i=0; i<len;i++){
+            hashVal = ((hashVal<<1) & all1) | (s.charAt(i)-'0');
+            if(i>=k-1 && !hash[hashVal]){
+                hash[hashVal] = true;
+                total--;
+                if(total==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean hasAllCodesUsingSet(String s, int k) {
         int len = s.length();
         if(len<(1<<k)){
             return false;
@@ -11,7 +35,6 @@ class Solution {
                 return true;
             }
         }
-        //System.out.println(set);
         
         return false;
     }
