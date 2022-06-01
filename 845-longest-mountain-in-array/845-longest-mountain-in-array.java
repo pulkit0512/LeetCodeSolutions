f3a5d22942dd516ae88ff606a46a96f1e27eec89
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-01 23:59:29 +0530

    Time: 5 ms (21.16%), Space: 52.4 MB (34.57%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.java
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.java
@@ -1,5 +1,33 @@
 class Solution {
     public int longestMountain(int[] arr) {
+        //return longestMountainUsingLeftRightIncreasingArrays(arr);
+        
+        //Follow Up using O(1) Space and in one pass
+        return longestMountainUsingConstantSpace(arr);
+    }
+    
+    private int longestMountainUsingConstantSpace(int[] arr) {
+        int n = arr.length;
+        int base = 0, ans = 0;
+        while(base<n) {
+            int end = base;
+            if(end+1<n && arr[end]<arr[end+1]){
+                while(end+1<n && arr[end]<arr[end+1]){
+                    end++;
+                }
+                if(end+1<n && arr[end]>arr[end+1]){
+                    while(end+1<n && arr[end]>arr[end+1]){
+                        end++;
+                    }
+                    ans = Math.max(ans, end-base+1);
+                }
+            }
+            base = Math.max(end, base+1);
+        }
+        return ans;
+    }
+    
+    private int longestMountainUsingLeftRightIncreasingArrays(int[] arr) {
         int n = arr.length;
         int left[] = new int[n];
         int right[] = new int[n];
