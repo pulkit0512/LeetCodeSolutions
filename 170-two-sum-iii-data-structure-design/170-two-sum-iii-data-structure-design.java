04505dd26dbc6d2943fd6957e9e5ff7f96b71b32
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-02 22:03:44 +0530

    Time: 100 ms (78.38%), Space: 49.4 MB (84.58%) - LeetHub

--- 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.java
+++ 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.java
@@ -7,9 +7,10 @@ class TwoSum {
     
     public void add(int number) {
         if(!map.containsKey(number)){
-            map.put(number, 0);
+            map.put(number, 1);
+        }else{
+            map.put(number, map.get(number)+1);
         }
-        map.put(number, map.get(number)+1);
     }
     
     public boolean find(int value) {
