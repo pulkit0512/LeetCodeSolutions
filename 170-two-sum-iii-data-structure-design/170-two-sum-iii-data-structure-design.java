4b57898a21b05759862d9b0b270ac0dc7a2bd53b
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-02 21:58:35 +0530

    Time: 149 ms (51.78%), Space: 66.1 MB (54.04%) - LeetHub

---
+++ 170-two-sum-iii-data-structure-design/170-two-sum-iii-data-structure-design.java
@@ -0,0 +1,34 @@
+class TwoSum {
+
+    Map<Integer, Integer> map;
+    public TwoSum() {
+        map = new HashMap<>();
+    }
+    
+    public void add(int number) {
+        if(!map.containsKey(number)){
+            map.put(number, 0);
+        }
+        map.put(number, map.get(number)+1);
+    }
+    
+    public boolean find(int value) {
+        for(int x:map.keySet()){
+            int next = value-x;
+            if(next==x && map.get(next)<2){
+                continue;
+            }
+            if(map.containsKey(value-x)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
