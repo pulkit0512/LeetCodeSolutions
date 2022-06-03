352381e72590d9c85c4627aa2900f945afeb0c97
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-04 00:36:49 +0530

    Time: 11 ms (8.62%), Space: 42.6 MB (57.58%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    String[] map;
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        int n = digits.length();
+        if(n==0){
+            return ans;
+        }
+        if(map==null) {
+            map = new String[10];
+            map[2] = "abc";
+            map[3] = "def";
+            map[4] = "ghi";
+            map[5] = "jkl";
+            map[6] = "mno";
+            map[7] = "pqrs";
+            map[8] = "tuv";
+            map[9] = "wxyz";
+        }
+        helper(ans, digits, 0, "", n);
+        return ans;
+    }
+    public void helper(List<String> ans, String digits, int idx, String s, int n) {
+        if(idx==n){
+            ans.add(s);
+            return;
+        }
+        int len = map[digits.charAt(idx)-'0'].length();
+        for(int i=0;i<len;i++){
+            helper(ans, digits, idx+1, s+map[digits.charAt(idx)-'0'].charAt(i), n);
+        }
+    }
+}
