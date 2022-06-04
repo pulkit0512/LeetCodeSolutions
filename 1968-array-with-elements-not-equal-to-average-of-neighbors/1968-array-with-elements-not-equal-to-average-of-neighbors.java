816d7b53c47ed9c4c70a0293e0b39f5e5ea001e4
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-04 15:07:20 +0530

    Time: 6 ms (93.57%), Space: 176.9 MB (6.43%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        //return rearrangeArrayUsingExtraSpace(nums);
+        return rearrangeArrayInPlace(nums);
+    }
+    private int[] rearrangeArrayUsingExtraSpace(int[] nums) {
         int[] ans = new int[nums.length];
         Arrays.sort(nums);
         int i = 0, j = nums.length-1;
@@ -10,5 +14,27 @@ class Solution {
             }
         }
         return ans;
+    }
+    private int[] rearrangeArrayInPlace(int[] nums) {
+        int dir = 0; // 0 indicates next element should be greater and 1 indicates next element should be smaller.
+        for(int i=0;i<nums.length-1;i++){
+            if(dir==0){
+                if(nums[i]>nums[i+1]){
+                    swap(nums, i, i+1);
+                }
+                dir = 1;
+            }else {
+                if(nums[i]<nums[i+1]){
+                    swap(nums, i, i+1);
+                }
+                dir = 0;
+            }
+        }
+        return nums;
+    }
+    private void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
