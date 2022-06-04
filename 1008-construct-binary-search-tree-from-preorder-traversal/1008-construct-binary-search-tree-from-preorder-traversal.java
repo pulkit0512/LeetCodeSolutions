96a4502eea8bff70e052b476d54ba95d7b8ba3c3
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-05 02:26:32 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (33.83%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -16,7 +16,34 @@
 class Solution {
     TreeNode root;
     int last;
+    int pre[];
     public TreeNode bstFromPreorder(int[] preorder) {
+        //return bstFromPreorderUsingLastPointerAndMaxLimit(preorder);
+        return bstFromPreorderUsingValidateBSTTechnique(preorder);
+    }
+    
+    private TreeNode bstFromPreorderUsingValidateBSTTechnique(int[] pre) {
+        last = 0;
+        this.pre = pre;
+        return getBst2(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    private TreeNode getBst2(int min, int max) {
+        if(last==pre.length){
+            return null;
+        }
+        int val = pre[last];
+        if(val<min || val>max){
+            return null;
+        }
+        last++;
+        TreeNode root = new TreeNode(val);
+        root.left = getBst2(min, val);
+        root.right = getBst2(val, max);
+        return root;
+    }
+    
+    private TreeNode bstFromPreorderUsingLastPointerAndMaxLimit(int[] preorder) {
         last = 0;
         getBst(preorder, 0, Integer.MAX_VALUE);
         return root;
