f3598a884e5784ddcb2c8919c96010cf1ecf6ce6
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-05 23:15:37 +0530

    Time: 25 ms (25.91%), Space: 58.6 MB (21.06%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -7,7 +7,7 @@ class Solution {
             for(int i=0;i<len;i++){
                 freqMap[str.charAt(i)-'a']++;
             }
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder(52);
             for(int i=0;i<26;i++){
                 sb.append(freqMap[i]).append('#');
             }
