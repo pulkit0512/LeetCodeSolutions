79e1c2a17962905061e79ef26cdb5a354594754a
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-05 17:36:44 +0530

    Time: 33 ms (86.24%), Space: 46.6 MB (86.36%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.java
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -1,5 +1,6 @@
 class Solution {
     int dir[][] = new int[][]{{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+    Map<String, Integer> map;
     class MoveData{
         int x, y, moves;
         public MoveData(int x, int y, int moves) {
@@ -9,6 +10,32 @@ class Solution {
         }
     }
     public int minKnightMoves(int x, int y) {
+        //return minKnightMovesBFS(x, y);
+        return minKnightMovesDFS(x, y);
+    }
+    
+    private int minKnightMovesDFS(int x, int y){
+        map = new HashMap<>();
+        return dfs(Math.abs(x), Math.abs(y));
+    }
+    
+    private int dfs(int x, int y) {
+        String key = x+","+y;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        if(x+y==0){
+            return 0;
+        }
+        if(x+y==2){
+            return 2;
+        }
+        int steps = Math.min(dfs(Math.abs(x-2), Math.abs(y-1)), dfs(Math.abs(x-1), Math.abs(y-2))) + 1;
+        map.put(key, steps);
+        return steps;
+    }
+    
+    private int minKnightMovesBFS(int x, int y) {
         Queue<MoveData> que = new LinkedList<>();
         boolean[][] visited = new boolean[601][601];
         que.add(new MoveData(0,0,0));
