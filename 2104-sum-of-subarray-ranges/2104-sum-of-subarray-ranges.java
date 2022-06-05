e6f7c32a536eb049f419a0fe346b4130bd9e525e
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-06 04:02:54 +0530

    Time: 22 ms (75.64%), Space: 45 MB (56.17%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,5 +1,44 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        //return subArrayRangesN2(nums);
+        return subArrayRangesN(nums);
+    }
+    
+    private long subArrayRangesN(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> maxStack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
+        long maxSum = 0, minSum = 0;
+        for(int i=0;i<=n;i++){
+            maxSum += getMaxSum(maxStack, nums, i);
+            minSum += getMinSum(minStack, nums, i);
+        }
+        return maxSum - minSum;
+    }
+    
+    private long getMaxSum(Stack<Integer> maxStack, int[] nums, int idx) {
+        long maxSum = 0;
+        while(!maxStack.isEmpty() && (idx==nums.length || nums[maxStack.peek()]<nums[idx])){
+            int pop_i = maxStack.pop();
+            int prev_i = maxStack.isEmpty()?-1:maxStack.peek();
+            maxSum += (idx-pop_i)*(pop_i-prev_i)*(long)nums[pop_i];
+        }
+        maxStack.push(idx);
+        return maxSum;
+    }
+    
+    private long getMinSum(Stack<Integer> minStack, int[] nums, int idx) {
+        long minSum = 0;
+        while(!minStack.isEmpty() && (idx==nums.length || nums[minStack.peek()]>nums[idx])){
+            int pop_i = minStack.pop();
+            int prev_i = minStack.isEmpty()?-1:minStack.peek();
+            minSum += (idx-pop_i)*(pop_i-prev_i)*(long)nums[pop_i];
+        }
+        minStack.push(idx);
+        return minSum;
+    }
+    
+    private long subArrayRangesN2(int[] nums) {
         int n = nums.length;
         long ans = 0;
         for(int i=2;i<=n;i++){
