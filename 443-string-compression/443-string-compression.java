d71120b0df9b407a2994070d3329272f973432ac
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-06 02:23:56 +0530

    Time: 1 ms (99.07%), Space: 44.2 MB (37.64%) - LeetHub

--- 443-string-compression/443-string-compression.java
+++ 443-string-compression/443-string-compression.java
@@ -3,25 +3,46 @@ class Solution {
         int prevCount = 1;
         char prevChar = chars[0];
         StringBuilder sb = new StringBuilder();
+        int idx = 0;
         for(int i=1;i<chars.length;i++){
             if(chars[i]==prevChar){
                 prevCount++;
             }else{
-                sb.append(prevChar);
+                chars[idx++] = prevChar;
+                //sb.append(prevChar);
                 if(prevCount>1){
-                    sb.append(prevCount);
+                    int st = idx;
+                    while(prevCount>0){
+                        chars[idx++] = (char)((prevCount%10)+'0');
+                        prevCount /= 10;
+                    }
+                    int ed = idx-1;
+                    reverse(chars, st, ed);
                 }
                 prevChar = chars[i];
                 prevCount = 1;
             }
         }
-        sb.append(prevChar);
+        chars[idx++] = prevChar;
         if(prevCount>1){
-            sb.append(prevCount);
+            int st = idx;
+            while(prevCount>0){
+                chars[idx++] = (char)((prevCount%10)+'0');
+                prevCount /= 10;
+            }
+            int ed = idx-1;
+            reverse(chars, st, ed);
         }
-        for(int i=0;i<sb.length();i++){
-            chars[i] = sb.charAt(i);
+        return idx;
+    }
+    
+    private void reverse(char[] chars, int st, int ed) {
+        while(st<ed){
+            char temp = chars[st];
+            chars[st] = chars[ed];
+            chars[ed] = temp;
+            st++;
+            ed--;
         }
-        return sb.length();
     }
 }
