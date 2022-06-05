02046a9264ec4435333b88a4114003cadbede21a
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-06 01:56:52 +0530

    Time: 22 ms (29.47%), Space: 50.8 MB (26.42%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,17 +1,12 @@
 class Solution {
-    class sort implements Comparator<Map.Entry<Integer, Integer>> {
-        public int compare(Map.Entry<Integer, Integer> a, Map.Entry<Integer, Integer> b) {
-            return (int)a.getValue() - (int)b.getValue();
-        }
-    }
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freqMap = new HashMap<>();
         for(int num:nums){
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> ((int)a.getValue() - (int)b.getValue()));
-        for(Map.Entry<Integer, Integer> entry:freqMap.entrySet()){
-            pq.add(entry);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (freqMap.get(a) - freqMap.get(b)));
+        for(int key:freqMap.keySet()){
+            pq.add(key);
             if(pq.size()>k){
                 pq.poll();
             }
@@ -19,7 +14,7 @@ class Solution {
         int ans[] = new int[k];
         int idx = 0;
         while(!pq.isEmpty()) {
-            ans[idx++] = pq.poll().getKey();
+            ans[idx++] = pq.poll();
         }
         return ans;
     }
