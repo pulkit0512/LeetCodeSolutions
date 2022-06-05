476aaca5da2ac98958aefd5c4637d883763b6d75
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-05 21:12:29 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (81.71%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,10 +1,16 @@
 class Solution {
+    // With atmost 4 iterations we can check if it is forming a cycle or not.
+    // If after any iteration steps is 0 then return true.
+    // If after exhausting all the 4 cycles if we don't get steps as 0 then return false.
+    // we can solve it in Single cycle as well, if after the cycle is completed if steps is 0 or dir is not north
+    // then cycle exist else false.
     public boolean isRobotBounded(String instructions) {
         int step = 0;
         int n = instructions.length();
         //0 = north, 1 = east, 2 = south, 3 = west
         int dir = 0;
-        for(int i=0, cycle=1;i<n && cycle<=4;i++){
+        //for(int i=0, cycle=1;i<n && cycle<=4;i++){
+        for(int i=0;i<n;i++){
             if(instructions.charAt(i)=='G'){
                 if(dir==0 || dir==1){
                     step++;
@@ -16,14 +22,14 @@ class Solution {
             }else{
                 dir = (dir+1)%4;
             }
-            if(i+1==n){
+            /*if(i+1==n){
                 if(step==0){
                     return true;
                 }
                 i=-1;
                 cycle++;
-            }
+            }*/
         }
-        return false;
+        return step==0 || dir!=0;
     }
 }
