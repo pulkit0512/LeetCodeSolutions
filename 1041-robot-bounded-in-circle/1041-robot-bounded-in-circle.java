dc9020a5713550e633ea5697ea5b8e055aea1fc9
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-05 21:02:46 +0530

    Time: 1 ms (64.22%), Space: 41.2 MB (71.79%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -5,38 +5,16 @@ class Solution {
         //0 = north, 1 = east, 2 = south, 3 = west
         int dir = 0;
         for(int i=0, cycle=1;i<n && cycle<=4;i++){
-            if(dir==0){
-                if(instructions.charAt(i)=='G'){
+            if(instructions.charAt(i)=='G'){
+                if(dir==0 || dir==1){
                     step++;
-                }else if(instructions.charAt(i)=='L'){
-                    dir = 3;
                 }else{
-                    dir = 1;
-                }
-            }else if(dir==1){
-                if(instructions.charAt(i)=='G'){
-                    step++;
-                }else if(instructions.charAt(i)=='L'){
-                    dir = 0;
-                }else{
-                    dir = 2;
-                }
-            }else if(dir==2){
-                if(instructions.charAt(i)=='G'){
                     step--;
-                }else if(instructions.charAt(i)=='L'){
-                    dir = 1;
-                }else{
-                    dir = 3;
                 }
+            }else if(instructions.charAt(i)=='L'){
+                dir = (dir+3)%4;
             }else{
-                if(instructions.charAt(i)=='G'){
-                    step--;
-                }else if(instructions.charAt(i)=='L'){
-                    dir = 2;
-                }else{
-                    dir = 0;
-                }
+                dir = (dir+1)%4;
             }
             if(i+1==n){
                 if(step==0){
