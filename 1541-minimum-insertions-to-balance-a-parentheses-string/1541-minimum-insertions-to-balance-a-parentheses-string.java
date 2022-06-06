8e2f72dba07b157db57af00dfd424c7a9def50ef
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-06 06:21:23 +0530

    Time: 10 ms (97.01%), Space: 42.5 MB (98.09%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -2,21 +2,20 @@ class Solution {
     public int minInsertions(String s) {
         int len = s.length();
         // Open to keep track of unbalanced open brackets.
-        // Close to keep track of unbalanced close brackets.
-        int open = 0, close = 0;
+        int open = 0, idx = 0;
         int ans = 0;
-        while(close<len){
-            if(s.charAt(close)=='('){
+        while(idx<len){
+            if(s.charAt(idx)=='('){
                 // increment count of open brackets.
                 open++;
             }else{
                 // If reached to end or next char is not close bracket.
                 // so we only get one close bracket. Need to insert one ')' in the String.
-                if(close+1 == len || s.charAt(close+1)!=')'){
+                if(idx+1 == len || s.charAt(idx+1)!=')'){
                     ans++;
                 }else{
                     // found both the close brackets.
-                    close++;
+                    idx++;
                 }
                 // Found both close brackets if we don't have any open bracket before we need to insert 1 open bracket.
                 if(open==0){
@@ -26,7 +25,7 @@ class Solution {
                     open--;
                 }
             }
-            close++;
+            idx++;
         }
         // Any open brackets still left and 2*open number of close brackets.
         return ans + 2*open;
