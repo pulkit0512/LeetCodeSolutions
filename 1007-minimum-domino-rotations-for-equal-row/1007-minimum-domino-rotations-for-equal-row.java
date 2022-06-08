dbe052de84dda2b25e19724fb309a1c6d3b6d8d3
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-09 00:56:53 +0530

    Time: 10 ms (22.25%), Space: 92.5 MB (32.45%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int[] topFreq = new int[7];
+        int[] bottomFreq = new int[7];
+        int[] topMap = new int[7];
+        int[] bottomMap = new int[7];
+        for(int i=0;i<tops.length;i++){
+            topFreq[tops[i]]++;
+            bottomFreq[bottoms[i]]++;
+            if(tops[i]!=bottoms[i]){
+                topMap[tops[i]]++;
+            }
+            if(tops[i]!=bottoms[i]){
+                bottomMap[bottoms[i]]++;
+            }
+        }
+        int topSwaps = tops.length+1;
+        for(int i=1;i<=6;i++){
+            if(topFreq[i]==tops.length){
+                return 0;
+            }
+            if(topFreq[i]+bottomMap[i]==tops.length){
+                topSwaps = Math.min(topSwaps, bottomMap[i]);
+            }
+        }
+        for(int i=1;i<=6;i++){
+            if(bottomFreq[i]==tops.length){
+                return 0;
+            }
+            if(bottomFreq[i]+topMap[i]==tops.length){
+                topSwaps = Math.min(topSwaps, topMap[i]);
+            }
+        }
+        if(topSwaps == tops.length+1){
+            return -1;
+        }
+        return topSwaps;
+    }
+}
+
+/*
+3 5 1 2 3
+3 6 3 3 4
+*/
