dcdfb27f4dcbf90c6222f979253569fe35e12670
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-11 16:28:42 +0530

    Time: 8 ms (61.34%), Space: 95.4 MB (66.90%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,5 +1,29 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
+        //return minOperationsNSpace(nums, x);
+        return minOperationsConstantSpace(nums, x);
+    }
+    
+    private int minOperationsConstantSpace(int[] nums, int x) {
+        int total = 0, min = Integer.MAX_VALUE;
+        for(int num:nums){
+            total += num;
+        }
+        int left = 0;
+        for(int right=0;right<nums.length;right++){
+            total -= nums[right];
+            while(total<x && left<=right){
+                total += nums[left];
+                left++;
+            }
+            if(total == x){
+                min = Math.min(min, left+(nums.length-1-right));
+            }
+        }
+        return min==Integer.MAX_VALUE?-1:min;
+    }
+    
+    private int minOperationsNSpace(int[] nums, int x) {
         int sum = 0;
         Map<Integer, Integer> cSum = new HashMap<>();
         cSum.put(0, 0);
