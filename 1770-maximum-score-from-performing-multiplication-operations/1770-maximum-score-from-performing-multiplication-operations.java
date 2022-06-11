3231c69c069d2600d12f52ae2530947b4c94374e
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-11 20:06:24 +0530

    Time: 66 ms (75.57%), Space: 125.3 MB (25.76%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,5 +1,19 @@
 class Solution {
-    int[][] dp;
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int m = multipliers.length;
+        int n = nums.length;
+        int[][] dp = new int[m+1][m+1];
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int ed = n - 1 - (i-j);
+                dp[i][j] = Math.max(multipliers[i]*nums[j] + dp[i+1][j+1], multipliers[i]*nums[ed] + dp[i+1][j]);
+            }
+        }
+        return dp[0][0];
+    }
+    
+    // DP with memoization
+    /*int[][] dp;
     int nums[], multipliers[];
     public int maximumScore(int[] nums, int[] multipliers) {
         dp = new int[1000][1000];
@@ -24,5 +38,5 @@ class Solution {
         
         dp[idx][st] = Math.max(left, right);
         return dp[idx][st];
-    }
+    }*/
 }
