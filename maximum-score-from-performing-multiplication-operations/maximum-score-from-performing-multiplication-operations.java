c090f18ee3560d89fd0de37f73feede0b3c1fc8a
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-11 20:19:54 +0530

    Time: 77 ms, Memory: 126.3 MB - LeetHub

---
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int m = multipliers.length;
+        int n = nums.length;
+        int dp[][] = new int[m+1][m+1];
+        for(int opr=m-1;opr>=0;opr--){
+            for(int left=0;left<=opr;left++){
+                int right = n - 1 - (opr-left);
+                dp[opr][left] = Math.max(multipliers[opr]*nums[left] + dp[opr+1][left+1],
+                                        multipliers[opr]*nums[right] + dp[opr+1][left]);
+            }
+        }
+        return dp[0][0];
+    }
+    
+    // DP with memoization
+    /*int[][] dp;
+    int nums[], multipliers[];
+    public int maximumScore(int[] nums, int[] multipliers) {
+        dp = new int[1000][1000];
+        this.nums = nums;
+        this.multipliers = multipliers;
+        return maximumScoreUtil(0, 0);
+    }
+    
+    private int maximumScoreUtil(int idx, int st) {
+        //System.out.println(st+" "+ed);
+        if(multipliers.length==idx){
+            return 0;
+        }
+        int ed = nums.length - 1 - (idx-st);
+        
+        if(dp[idx][st]!=0){
+            return dp[idx][st];
+        }
+        
+        int left = nums[st]*multipliers[idx] + maximumScoreUtil(idx+1, st+1);
+        int right = nums[ed]*multipliers[idx] + maximumScoreUtil(idx+1, st);
+        
+        dp[idx][st] = Math.max(left, right);
+        return dp[idx][st];
+    }*/
+}
