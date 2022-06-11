cb754d55e31268e1506422ed11f347716ad38f04
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-11 19:48:27 +0530

    Time: 1713 ms (5.06%), Space: 192.1 MB (15.44%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,11 +1,14 @@
 class Solution {
     int[][] dp;
+    int nums[], multipliers[];
     public int maximumScore(int[] nums, int[] multipliers) {
         dp = new int[1000][1000];
-        return maximumScoreUtil(nums, multipliers, 0, 0);
+        this.nums = nums;
+        this.multipliers = multipliers;
+        return maximumScoreUtil(0, 0);
     }
     
-    private int maximumScoreUtil(int[] nums, int[] multipliers, int idx, int st) {
+    private int maximumScoreUtil(int idx, int st) {
         //System.out.println(st+" "+ed);
         if(multipliers.length==idx){
             return 0;
@@ -16,8 +19,8 @@ class Solution {
             return dp[idx][st];
         }
         
-        int left = nums[st]*multipliers[idx] + maximumScoreUtil(nums, multipliers, idx+1, st+1);
-        int right = nums[ed]*multipliers[idx] + maximumScoreUtil(nums, multipliers, idx+1, st);
+        int left = nums[st]*multipliers[idx] + maximumScoreUtil(idx+1, st+1);
+        int right = nums[ed]*multipliers[idx] + maximumScoreUtil(idx+1, st);
         
         dp[idx][st] = Math.max(left, right);
         return dp[idx][st];
