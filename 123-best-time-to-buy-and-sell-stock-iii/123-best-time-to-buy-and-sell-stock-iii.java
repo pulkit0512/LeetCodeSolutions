befc57c83d84f10cbb92f41afd55218d94886a99
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-12 19:00:13 +0530

    Time: 11 ms (35.37%), Space: 76.9 MB (66.51%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        return maxProfitNSpace(prices);
+    }
+    
+    private int maxProfitNSpace(int[] prices) {
+        int n = prices.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        left[0] = 0;
+        right[n-1] = 0;
+        int minLeft = prices[0], maxLeft = prices[0];
+        int minRight = prices[n-1], maxRight = prices[n-1];
+        for(int i=1;i<n;i++){
+            if(prices[i]<minLeft){
+                minLeft = prices[i];
+                maxLeft = prices[i];
+            }else if(prices[i]>maxLeft){
+                maxLeft = prices[i];
+            }
+            
+            if(prices[n-1-i]>maxRight){
+                maxRight = prices[n-1-i];
+                minRight = prices[n-1-i];
+            }else if(prices[n-1-i]<minRight){
+                minRight = prices[n-1-i];
+            }
+            
+            left[i] = Math.max(left[i-1], maxLeft-minLeft);
+            right[n-1-i] = Math.max(right[n-i], maxRight-minRight);
+        }
+        int maxProfit = 0;
+        for(int i=0;i<n;i++){
+            maxProfit = Math.max(maxProfit, left[i]+right[i]);
+        }
+        return maxProfit;
+    }
+}
