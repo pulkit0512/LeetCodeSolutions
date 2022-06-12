0136b2c7d5b00c87cc7c306077d57b7c6b615940
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-12 19:44:00 +0530

    Time: 15 ms (21.58%), Space: 45.4 MB (19.98%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        return maxProfitKSpace(k, prices);
+        //return maxProfitKSpace(k, prices);
+        return maxProfitNKSpace(k, prices);
     }
     
     private int maxProfitKSpace(int k, int[] prices) {
@@ -16,5 +17,27 @@ class Solution {
         }
         
         return sell[k];
+    }
+    
+    private int maxProfitNKSpace(int k, int[] prices) {
+        int n = prices.length;
+        //3rd dimension to store weather we are holding a stock or not
+        // 0 indicates not holding, 1 indicates holding;
+        int[][][] dp= new int[n+1][k+1][2];
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=k;j++){
+                for(int hold=0;hold<2;hold++){
+                    int doNothing = dp[i+1][j][hold];
+                    int doSomething;
+                    if(hold==1){
+                        doSomething = prices[i] + dp[i+1][j-1][0];
+                    }else{
+                        doSomething = -prices[i] + dp[i+1][j][1];
+                    }
+                    dp[i][j][hold] = Math.max(doNothing, doSomething);
+                }
+            }
+        }
+        return dp[0][k][0];
     }
 }
