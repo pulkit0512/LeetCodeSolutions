022b73a823d767b311a0e1a4ad39f4b86b375b89
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-12 08:33:32 +0530

    Time: 46 ms (17.81%), Space: 42.7 MB (18.38%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    int dp[][];
+    int[] jobDifficulty;
+    int len;
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        dp = new int[300][11];
+        for(int i=0;i<300;i++){
+            for(int j=0;j<=10;j++){
+                dp[i][j] = -1;
+            }
+        }
+        this.jobDifficulty = jobDifficulty;
+        this.len = jobDifficulty.length;
+        if(d>this.len){
+            return -1;
+        }
+        return minDifficultyUtil(0, 1, d);
+    }
+    
+    private int minDifficultyUtil(int idx, int day, int d) {
+        if(dp[idx][day]!=-1){
+            return dp[idx][day];
+        }
+        if(day==d){
+            int val = 0;
+            for(int i=idx;i<len;i++){
+                val = Math.max(val, jobDifficulty[i]);
+            }
+            return val;
+        }
+        int val = Integer.MAX_VALUE, curr = 0;
+        for(int i=idx;i<len-(d-day);i++){
+            curr = Math.max(curr, jobDifficulty[i]);
+            val = Math.min(val, curr + minDifficultyUtil(i+1, day+1, d));
+            //System.out.println(val+" "+i+" "+day+" "+curr);
+        }
+        dp[idx][day] = val;
+        return val;
+    }
+}
