a2a4fd3b19977f8cd85da55eb8e8f052bd3ef24c
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-12 17:12:55 +0530

    Time: 3 ms, Memory: 44.7 MB - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,6 +1,27 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return lengthOfLisN2(nums);
+        //return lengthOfLisN2(nums);
+        return lengthOfLisNLogN(nums);
+    }
+    
+    private int lengthOfLisNLogN(int[] nums) {
+        int n = nums.length;
+        int lis[] = new int[n];
+        lis[0] = nums[0];
+        int len = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>lis[len-1]){
+                lis[len] = nums[i];
+                len++;
+            }else{
+                int idx = Arrays.binarySearch(lis, 0, len-1, nums[i]);
+                if(idx<0){
+                    idx = -(idx+1);
+                }
+                lis[idx] = nums[i];
+            }
+        }
+        return len;
     }
     
     private int lengthOfLisN2(int[] nums) {
