7fc38149a9e1037b08d720ba1235a1411eb8946f
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-12 14:35:16 +0530

    Time: 4 ms, Memory: 42.8 MB - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -15,4 +15,30 @@ class Solution {
         }
         return dp[len-1];
     }
+    
+    /*
+    //Using set index of prev substring start that are in wordDict
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        List<Integer> index = new ArrayList<>();
+        int st = 0;
+        int len = s.length();
+        for(int i=1;i<=len;i++){
+            String sub = s.substring(st, i);
+            if(wordSet.contains(sub)){
+                index.add(st);
+                st = i;
+            }else{
+                for(int j=0;j<index.size();j++){
+                    if(wordSet.contains(s.substring(index.get(j), i))){
+                        index.add(st);
+                        st = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return st==len;
+    }
+    */
 }
