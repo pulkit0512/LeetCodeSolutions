39dc0e710771ef907f866b7870782642e6302ce9
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-14 00:38:00 +0530

    Time: 0 ms, Memory: 40.9 MB - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -17,3 +17,12 @@ class Solution {
         return dp[n-1];
     }
 }
+
+/*
+In the process to move from the upper left corner to the lower right corner, we need to move m + n - 2 times. Among them, there are m – 1 moves down and n – 1 moves to the right. Therefore, the total number of possible paths is equal to the number of combinations for selecting m – 1 downward moves from m + n – 2 total moves, and the number of combinations can be calculated as,
+
+(m+n-2) C (m-1) = (m+n-2)!/((m-1)! * (n-1)!)
+
+## time complexity: O(M)
+## Space Complexity: O(1)
+*/
