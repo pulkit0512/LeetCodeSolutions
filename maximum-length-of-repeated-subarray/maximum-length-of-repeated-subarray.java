01ddc224951d5a0e28d26fed7ac03f9a6b5147f9
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-16 16:35:14 +0530

    Time: 98 ms, Memory: 44.9 MB - LeetHub

---
+++ maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int dpPrev[] = new int[m+1];
+        int dpCurr[] = new int[m+1];
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]==nums2[j-1]){
+                    dpCurr[j] = dpPrev[j-1]+1;
+                }else{
+                    dpCurr[j] = 0;
+                }
+            }
+            for(int j=1;j<=m;j++){
+                dpPrev[j] = dpCurr[j];
+                ans = Math.max(ans, dpPrev[j]);
+            }
+        }
+        return ans;
+    }
+}
