97aaacb90ee3669dce347079bb816d8cf30a437d
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-19 03:50:30 +0530

    Time: 590 ms (62.12%), Space: 184 MB (20.46%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long distinctNames(String[] ideas) {
+        List<String>[] preWord = new ArrayList[26];
+        for(int i=0;i<26;i++){
+            preWord[i] = new ArrayList<>();
+        }
+        for(String idea:ideas){
+            String sub = idea.substring(1);
+            preWord[idea.charAt(0)-'a'].add(sub);
+        }
+        long ans = 0;
+        for(int i=0;i<26;i++){
+            for(int j=i+1;j<26;j++){
+                Set<String> set = new HashSet<>();
+                set.addAll(preWord[i]);
+                set.addAll(preWord[j]);
+                
+                ans += (set.size()-preWord[i].size())*(set.size()-preWord[j].size());
+            }
+        }
+        return 2*ans;
+    }
+}
