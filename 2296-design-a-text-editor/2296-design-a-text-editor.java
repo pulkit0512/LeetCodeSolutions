19d2e89fdc91efe2666a6cea6db012f9dd99e2bd
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-20 01:50:33 +0530

    Time: 448 ms (23.12%), Space: 139.2 MB (36.61%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.java
+++ 2296-design-a-text-editor/2296-design-a-text-editor.java
@@ -8,30 +8,24 @@ class TextEditor {
     }
     
     public void addText(String text) {
-        String last = this.text.substring(cur);
-        this.text.delete(cur, this.text.length());
-        this.text.append(text).append(last);
-        cur = cur + text.length();
-        //System.out.println(this.text+" ADD "+cur);
+        this.text.insert(cur, text);
+        cur += text.length();
     }
     
     public int deleteText(int k) {
         int prev = cur;
         text.delete(Math.max(0,cur-k), cur);
         cur = Math.max(0, cur-k);
-        //System.out.println(text+" DELETE "+cur);
         return prev-cur;
     }
     
     public String cursorLeft(int k) {
         cur = Math.max(cur - k, 0);
-        //System.out.println(text+" CL "+cur);
         return text.substring(Math.max(0, cur-10), cur);
     }
     
     public String cursorRight(int k) {
         cur = Math.min(cur + k, text.length());
-        //System.out.println(text+" CR "+cur);
        return text.substring(Math.max(0, cur-10), cur);
     }
 }
