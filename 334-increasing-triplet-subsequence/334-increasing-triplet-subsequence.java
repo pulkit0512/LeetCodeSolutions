853b957121cc53a29e787410674f5efaebc544d2
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-21 00:39:12 +0530

    Time: 1677 ms (5.14%), Space: 115.6 MB (5.18%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if(n<=2){
+            return false;
+        }
+        int ans[] = new int[3];
+        ans[0] = nums[0];
+        int len = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>ans[len-1]){
+                ans[len] = nums[i];
+                len++;
+            }else{
+                int idx;
+                if(len==1){
+                    idx = 0;
+                }else{
+                    if(nums[i]<=ans[0]){
+                        idx = 0;
+                    }else{
+                        idx = 1;
+                    }
+                }
+                ans[idx] = nums[i];
+            }
+            System.out.println(ans[0]+" "+ans[1]+" "+ans[2]);
+            if(len==3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
