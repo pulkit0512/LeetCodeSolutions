8a390d1183a57c2955d5c14d9cddc1e6346b44fe
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-22 01:55:38 +0530

    Time: 1 ms (74.83%), Space: 44.7 MB (65.37%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -23,6 +23,42 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        //return connectBFS(root);
+        return connectUsingNext(root);
+    }
+    
+    Node leftMost, nextLevelNode;
+    private Node connectUsingNext(Node root) {
+        if(root==null){
+            return null;
+        }
+        leftMost = root;
+        while(leftMost!=null){
+            Node cur = leftMost;
+            nextLevelNode = null;
+            leftMost = null;
+            while(cur!=null){
+                processChild(cur.left);
+                processChild(cur.right);
+                cur = cur.next;
+            }
+        }
+        return root;
+    }
+    
+    private void processChild(Node childNode) {
+        if(childNode==null){
+            return;
+        }
+        if(nextLevelNode!=null){
+            nextLevelNode.next = childNode;
+        }else{
+            leftMost = childNode;
+        }
+        nextLevelNode = childNode;
+    }
+    
+    private Node connectBFS(Node root) {
         if(root==null){
             return null;
         }
