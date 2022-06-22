c095f72794550f4824c6d0137b4a59ec17d4dcc3
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-23 03:07:31 +0530

    Time: 83 ms (37.03%), Space: 54.9 MB (38.11%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,67 +1,35 @@
 class Solution {
-    class StepState {
-        public int steps, row, col, k;
-
-        public StepState(int row, int col, int k, int steps) {
-            this.steps = steps;
-            this.row = row;
-            this.col = col;
-            this.k = k;
-        }
-
-        @Override
-        public int hashCode() {
-            // needed when we put objects into any container class
-            return (this.row + 1) * (this.col + 1) * this.k;
-        }
-
-        @Override
-        public boolean equals(Object other) {
-            /**
-            * only (row, col, k) matters as the state info
-            */
-            if (!(other instanceof StepState)) {
-                return false;
-            }
-            StepState newState = (StepState) other;
-            return (this.row == newState.row) && (this.col == newState.col) && (this.k == newState.k);
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%d %d %d", this.row, this.col, this.k);
-        }
-    }
     int dir[][] = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int shortestPath(int[][] grid, int k) {
-        Queue<StepState> pq = new LinkedList<>();
-        Set<StepState> set = new HashSet<>();
+        Queue<int[]> pq = new LinkedList<>();
+        Set<String> set = new HashSet<>();
         int m = grid.length;
         int n = grid[0].length;
         if(k>=m+n-2){
             return m+n-2;
         }
-        StepState start = new StepState(0,0,0,0);
-        pq.add(start);
-        set.add(start);
+        pq.add(new int[]{0,0,0,0});
+        set.add("0_0_0");
         int minSteps = Integer.MAX_VALUE;
         while(!pq.isEmpty()){
-            StepState state= pq.poll();
-            if(state.row==m-1 && state.col==n-1){
-                minSteps = state.steps;
+            int cur[] = pq.poll();
+            if(cur[0]==m-1 && cur[1]==n-1){
+                minSteps = cur[3];
                 break;
             }
             for(int i=0;i<4;i++){
-                int newRow = dir[i][0] + state.row;
-                int newCol = dir[i][1] + state.col;
+                int newRow = dir[i][0] + cur[0];
+                int newCol = dir[i][1] + cur[1];
                 if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
-                    StepState s0 = new StepState(newRow, newCol, state.k, state.steps+1);
-                    StepState s1 = new StepState(newRow, newCol, state.k+1, state.steps+1);
+                    String s0 = new StringBuilder().append(newRow)
+                        .append('_').append(newCol).append('_').append(cur[2]).toString();
+                    String s1 = new StringBuilder().append(newRow)
+                        .append('_').append(newCol).append('_').append(cur[2]+1).toString();
                     if(grid[newRow][newCol]==0 && !set.contains(s0)){
-                        pq.add(s0);
+                        pq.add(new int[]{newRow, newCol, cur[2], cur[3]+1});
                         set.add(s0);
-                    }else if(grid[newRow][newCol]==1 && state.k+1<=k && !set.contains(s1)){
-                        pq.add(s1);
+                    }else if(grid[newRow][newCol]==1 && cur[2]+1<=k && !set.contains(s1)){
+                        pq.add(new int[]{newRow, newCol, cur[2]+1, cur[3]+1});
                         set.add(s1);
                     }
                 }
