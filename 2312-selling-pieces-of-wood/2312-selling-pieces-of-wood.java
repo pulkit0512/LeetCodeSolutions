dd45249abb72343bbef4dd1ad88d5599d6adab05
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-22 20:59:18 +0530

    Time: 78 ms (62.57%), Space: 54.4 MB (40.12%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.java
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.java
@@ -1,5 +1,30 @@
 class Solution {
     public long sellingWood(int m, int n, int[][] prices) {
+        //return sellingWoodTopDown(m, n, prices);
+        return sellingWoodBottomUp(m, n, prices);
+    }
+    
+    private long sellingWoodBottomUp(int m, int n, int[][] prices) {
+        int p[][] = new int[m+1][n+1];
+        for(int price[]:prices){
+            p[price[0]][price[1]] = price[2];
+        }
+        long dp[][] = new long[m+1][n+1];
+        for(int h=1;h<=m;h++){
+            for(int w=1;w<=n;w++){
+                dp[h][w] = p[h][w];
+                for(int x=1;x<=w/2;x++){
+                    dp[h][w] = Math.max(dp[h][w], dp[h][x] + dp[h][w-x]);
+                }
+                for(int x=1;x<=h/2;x++){
+                    dp[h][w] = Math.max(dp[h][w], dp[x][w] + dp[h-x][w]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    private long sellingWoodTopDown(int m, int n, int[][] prices) {
         long memo[][] = new long[m+1][n+1];
         for(int i=0;i<=m;i++){
             Arrays.fill(memo[i], -1);
