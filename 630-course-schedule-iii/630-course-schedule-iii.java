9cc3366df66c4814cd520ae958f5244b50300d84
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-23 20:01:16 +0530

    Time: 50 ms (69.26%), Space: 75.1 MB (14.26%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,17 +1,8 @@
 class Solution {
-    class Sort implements Comparator<int[]>{
-        public int compare(int[] a, int[] b){
-            if(a[1]==b[1]){
-                return a[0]-b[0];
-            }
-            return a[1]-b[1];
-        }
-    }
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses, (a,b)->(a[1]-b[1]));
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int sum = 0;
-        int cnt = 0, ans = 0;
         for(int i=0;i<courses.length;i++){
             if(courses[i][0]>courses[i][1]){
                 continue;
@@ -19,19 +10,13 @@ class Solution {
             if(sum+courses[i][0]<=courses[i][1]){
                 sum += courses[i][0];
                 pq.add(courses[i][0]);
-                cnt++;
             }else{
-                ans = Math.max(ans, cnt);
-                sum += courses[i][0];
-                pq.add(courses[i][0]);
-                cnt++;
-                while(!pq.isEmpty() && sum>courses[i][1]){
-                    sum -= pq.poll();
-                    cnt--;
+                if(!pq.isEmpty() && pq.peek()>courses[i][0]){
+                    sum += courses[i][0] - pq.poll();
+                    pq.add(courses[i][0]);
                 }
             }
         }
-        ans = Math.max(ans, cnt);
-        return ans;
+        return pq.size();
     }
 }
