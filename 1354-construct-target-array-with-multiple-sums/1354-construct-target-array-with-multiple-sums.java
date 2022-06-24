f01e63a7f77330233af963254079d125ad81659e
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-24 18:27:57 +0530

    Time: 9 ms (93.94%), Space: 49.6 MB (94.95%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isPossible(int[] target) {
-        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Long> pq = new PriorityQueue<>(target.length, Collections.reverseOrder());
         long sum = 0;
         for(int tar:target){
             pq.add((long)tar);
             sum += tar;
         }
         while(pq.peek()!=1){
-            long x = pq.poll();
-            long rest = sum-x;
+            long max = pq.poll();
+            long rest = sum-max;
+            if(rest<=0 || max<=rest){
+                return false;
+            }
             if(rest==1){
                 return true;
             }
-            if(rest<=0 || x<=rest){
-                return false;
-            }
-            long newX = x%rest;
+            long newX = max%rest;
             sum = rest + newX;
             pq.add(newX);
         }
