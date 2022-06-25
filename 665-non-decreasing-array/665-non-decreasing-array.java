1fcf0afab0ebde59b4ab7d5320b79a2a33ee6204
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-25 17:32:41 +0530

    Time: 1 ms (97.53%), Space: 43.8 MB (81.86%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,15 +1,20 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
         int modified = 0;
-        for(int i=1;i<nums.length;i++){
+        int n = nums.length;
+        for(int i=1;i<n;i++){
             if(nums[i]<nums[i-1]){
-                if((i+1<nums.length && nums[i+1]>=nums[i-1]) || (i+1>=nums.length) || (i-2>=0 && nums[i]>=nums[i-2]) || (i-2<0)){
+                if((i+1<n && nums[i+1]>=nums[i-1]) || (i+1>=n) || (i-2>=0 && nums[i]>=nums[i-2]) || (i-2<0)){
                     modified++;
                 }else{
+                    modified +=2;
+                }
+                
+                if(modified>1){
                     return false;
                 }
             }
         }
-        return modified<=1;
+        return true;
     }
 }
