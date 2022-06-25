3550a5c48f1bb9f50a929b7629efa6f80746d7e8
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-26 00:26:05 +0530

    Time: 430 ms (27.95%), Space: 43.1 MB (49.36%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,35 +1,36 @@
 class Solution {
     public int largestVariance(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        int len = rev.length();
+        int map[] = new int[26];
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            map[s.charAt(i)-'a']++;
+        }
         int variance = 0;
         for(char i='a';i<='z';i++){
+            if(map[i-'a']==0){
+                continue;
+            }
             for(char j='a';j<='z';j++){
-                if(i==j){
+                if(map[j-'a']==0 || i==j){
                     continue;
                 }
-                variance = Math.max(variance, getVariance(s, len, i, j));
-                variance = Math.max(variance, getVariance(rev, len, i, j));
-            }
-        }
-        return variance;
-    }
-    
-    private int getVariance(String s, int len, char i, char j) {
-        int variance = 0;
-        int firstFreq = 0, secondFreq = 0;
-        for(int k=0;k<len;k++){
-            if(s.charAt(k)==i){
-                firstFreq++;
-            }else if(s.charAt(k)==j){
-                secondFreq++;
-            }
-                if(firstFreq>0 && secondFreq>0 && firstFreq>secondFreq){
-                variance = Math.max(variance, firstFreq-secondFreq);
-            }
-            if(firstFreq<secondFreq){
-                firstFreq = 0;
-                secondFreq = 0;
+                int firstFreq = 0, secondFreq = 0;
+                int secondCount = map[j-'a'];
+                for(int k=0;k<len;k++){
+                    if(s.charAt(k)==i){
+                        firstFreq++;
+                    }else if(s.charAt(k)==j){
+                        secondFreq++;
+                        secondCount--;
+                    }
+                    if(firstFreq>0 && secondFreq>0 && firstFreq>secondFreq){
+                        variance = Math.max(variance, firstFreq-secondFreq);
+                    }
+                    if(firstFreq<secondFreq && secondCount>0){
+                        firstFreq = 0;
+                        secondFreq = 0;
+                    }
+                }
             }
         }
         return variance;
