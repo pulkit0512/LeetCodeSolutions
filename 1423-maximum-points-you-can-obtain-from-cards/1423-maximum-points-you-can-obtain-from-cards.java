9bff951eadbfe7cdb99bc9341fa2f7150e8887a6
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-26 12:49:44 +0530

    Time: 2 ms (94.06%), Space: 65.7 MB (32.50%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+        //return maxScoreNSpace(cardPoints, k);
+        return maxScoreConstantSpace(cardPoints, k);
+    }
+    private int maxScoreNSpace(int[] cardPoints, int k) {
         int n = cardPoints.length;
         int preSum[] = new int[n+1];
         for(int i=1;i<=n;i++){
@@ -11,6 +15,20 @@ class Solution {
             //preSum[k-(n-i)] gives sum for elements from left boundary
             //since we can cross boundary, for k=8 if we get 2 from right then we fetch 6 from left.
             ans = Math.max(ans, (preSum[n]-preSum[i])+preSum[k-(n-i)]);
+        }
+        return ans;
+    }
+    private int maxScoreConstantSpace(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int leftScore = 0, rightScore = 0;
+        for(int i=0;i<k;i++){
+            leftScore += cardPoints[i];
+        }
+        int ans = leftScore;
+        for(int i=k-1;i>=0;i--){
+            rightScore += cardPoints[n-(k-i)];
+            leftScore -= cardPoints[i];
+            ans = Math.max(ans, leftScore+rightScore);
         }
         return ans;
     }
