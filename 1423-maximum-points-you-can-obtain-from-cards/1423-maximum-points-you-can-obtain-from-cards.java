b4d996486caa990a6114c875460b33ba68468d76
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-26 12:42:18 +0530

    Time: 6 ms (9.64%), Space: 66.6 MB (9.71%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -7,7 +7,10 @@ class Solution {
         }
         int ans = preSum[k];
         for(int i=n-1;i>=n-k;i--){
-            ans = Math.max(ans, preSum[n]-preSum[i]+preSum[k-(n-i)]);
+            //preSum[n]-preSum[i] gives sum for elements from right boundary
+            //preSum[k-(n-i)] gives sum for elements from left boundary
+            //since we can cross boundary, for k=8 if we get 2 from right then we fetch 6 from left.
+            ans = Math.max(ans, (preSum[n]-preSum[i])+preSum[k-(n-i)]);
         }
         return ans;
     }
