59e6654ff935525fa504179c92347af68291bb82
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-30 06:42:33 +0530

    Time: 2 ms (99.90%), Space: 42.7 MB (95.16%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,12 +1,48 @@
 class Solution {
     public int minMoves2(int[] nums) {
         //int mid = getMidElementNLogN(nums);
-        int mid = getMidElementNLogMid(nums);
+        //int mid = getMidElementNLogMid(nums);
+        int mid = quickSelect(nums, 0, nums.length-1, nums.length/2);
         int moves = 0;
         for(int num:nums){
             moves += Math.abs(num-mid);
         }
         return moves;
+    }
+    
+    private int quickSelect(int[] nums, int st, int ed, int kSmall) {
+        if(st==ed){
+            return nums[st];
+        }
+        Random rand = new Random();
+        int pivot = st + rand.nextInt(ed-st);
+        pivot = partition(nums, pivot, st, ed);
+        if(pivot == kSmall){
+            return nums[pivot];
+        }else if(pivot>kSmall){
+            return quickSelect(nums, st, pivot-1, kSmall);
+        }else{
+            return quickSelect(nums, pivot+1, ed, kSmall);
+        }
+    }
+    
+    private int partition(int[] nums, int pivot, int left, int right) {
+        int idx = left;
+        swap(nums, pivot, right);
+        for(int i=left;i<right;i++){
+            if(nums[i]<nums[right]){
+                swap(nums, i, idx);
+                idx++;
+            }
+        }
+        swap(nums, idx, right);
+        return idx;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
     private int getMidElementNLogN(int nums[]){
