1253e363d37099802937e3d33c82e0a12fc6024b
Pulkit Arora <arorapulkit2@gmail.com>
2022-06-30 06:30:04 +0530

    Time: 16 ms (11.66%), Space: 47.2 MB (6.72%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,11 +1,29 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);
-        int mid = nums[nums.length/2];
+        //int mid = getMidElementNLogN(nums);
+        int mid = getMidElementNLogMid(nums);
         int moves = 0;
         for(int num:nums){
             moves += Math.abs(num-mid);
         }
         return moves;
+    }
+    
+    private int getMidElementNLogN(int nums[]){
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+    
+    private int getMidElementNLogMid(int nums[]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = nums.length;
+        int mid = (int)Math.ceil(n/2.0);
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>mid){
+                pq.poll();
+            }
+        }
+        return pq.peek();
     }
 }
