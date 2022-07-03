7c667c1ef280c23b4f4f7cd99003d3c47edb7fa6
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-04 01:05:20 +0530

    Time: 7 ms (14.26%), Space: 41.3 MB (74.21%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,5 +1,10 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
+        //return wiggleMaxLengthN(nums);
+        return wiggleMaxLengthN2(nums);
+    }
+    
+    private int wiggleMaxLengthN(int[] nums) {
         int n = nums.length;
         if(n<=1){
             return n;
@@ -13,5 +18,31 @@ class Solution {
             }
         }
         return Math.max(inc, dec);
+    }
+    
+    private int wiggleMaxLengthN2(int[] nums) {
+        int n = nums.length;
+        if(n<=1){
+            return n;
+        }
+        int dpG[] = new int[n];
+        int dpS[] = new int[n];
+        dpS[0] = 1;
+        dpG[0] = 1;
+        int omax = 1;
+        for(int i=1;i<n;i++){
+            int maxS = 0, maxG = 0;
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    maxG = Math.max(maxG, dpS[j]+1);
+                }else if(nums[i]<nums[j]){
+                    maxS = Math.max(maxS, dpG[j]+1);
+                }
+            }
+            dpG[i] = maxG;
+            dpS[i] = maxS;
+            omax = Math.max(omax, Math.max(dpG[i], dpS[i]));
+        }
+        return omax;
     }
 }
