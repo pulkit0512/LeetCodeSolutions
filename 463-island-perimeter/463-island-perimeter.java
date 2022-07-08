a6b9f459e72e467b5f8c9de739e89d35f66c0d27
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-09 04:04:24 +0530

    Time: 8 ms (90.15%), Space: 63.4 MB (15.50%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -1,41 +1,32 @@
-class Solution {
-    public int islandPerimeter(int[][] grid) {
-        int cntOne = 0;
-        int hor = 0, ver = 0;
-        for(int i=0;i<grid.length;i++){
-            int horCnt = 0;
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1){
-                    cntOne++;
-                    horCnt++;
-                }else {
-                    if(horCnt>0){
-                        hor += horCnt-1;
-                        horCnt = 0;
-                    }
+public class Solution {
+   public int islandPerimeter(int[][] grid) {
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        int up, down, left, right;
+        int result = 0;
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    if (r == 0) { up = 0; }
+                    else { up = grid[r-1][c]; }
+                     
+                    if (c == 0) { left = 0; } 
+                    else { left = grid[r][c-1]; }
+                        
+                    if (r == rows-1) { down = 0; }
+                    else { down = grid[r+1][c]; }
+                        
+                    if (c == cols-1) { right = 0; }
+                    else { right = grid[r][c+1]; }
+                        
+                    result += 4-(up+left+right+down);
                 }
             }
-            if(horCnt>0){
-                hor += horCnt-1;
-            }
         }
-        for(int j=0;j<grid[0].length;j++){
-            int verCnt = 0;
-            for(int i=0;i<grid.length;i++){
-                if(grid[i][j]==1){
-                    verCnt++;
-                }else {
-                    if(verCnt>0){
-                        ver += verCnt-1;
-                        verCnt = 0;
-                    }
-                }
-            }
-            if(verCnt>0){
-                ver += verCnt-1;
-            }
-        }
-        //System.out.println(hor+" "+ver);
-        return 4*cntOne - 2*(hor+ver);
+
+        return result;
     }
 }
