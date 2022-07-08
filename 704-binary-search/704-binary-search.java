d873ad6803d0dc81904a972a81ad434a2b554069
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-09 02:49:23 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (84.23%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int st = 0, ed = nums.length-1;
+        while(st<=ed){
+            int mid = (st+ed)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                st = mid+1;
+            }else{
+                ed = mid-1;
+            }
+        }
+        return -1;
+    }
+}
