375b4f46dc6496f65854f599c14a043ab0403967
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-09 19:44:22 +0530

    Time: 41 ms (74.71%), Space: 114.7 MB (30.69%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -5,7 +5,7 @@ class Solution {
         dq.addLast(n-1);
         for(int i=1;i<n;i++){
             nums[n-1-i] += nums[dq.peekFirst()];
-            if(!dq.isEmpty() && dq.peekFirst()-(n-1-i)>=k){
+            if(!dq.isEmpty() && dq.peekFirst()-(n-1-i)==k){
                 dq.pollFirst();
             }
             while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[n-1-i]){
