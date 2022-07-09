8b9451ddf671b690137c6cb882399dfd76099836
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-09 21:38:37 +0530

    Time: 177 ms (11.11%), Space: 134.2 MB (11.11%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(buses);
+        Arrays.sort(passengers);
+        int idx = 0;
+        for(int i=0;i<passengers.length;i++){
+            int time = passengers[i];
+            while(idx!=buses.length && time>buses[idx]){
+                idx++;
+            }
+            if(idx==buses.length){
+                break;
+            }
+            map.put(buses[idx], map.getOrDefault(buses[idx], 0) + 1);
+            list.add(time);
+            set.add(time);
+            if(map.get(buses[idx])==capacity){
+                idx++;
+                if(idx==buses.length){
+                    break;
+                }
+            }
+        }
+        System.out.println(map);
+        System.out.println(list);
+        int ans = 1;
+        for(int i=list.size()-1;i>=0;i--){
+            int upper = list.get(i);
+            int lower = (i-1>=0?list.get(i-1):0);
+            if(upper-lower>1){
+                ans = upper-1;
+                break;
+            }
+        }
+        for(int i=buses.length-1;i>=0;i--){
+            if((map.containsKey(buses[i]) && map.get(buses[i])==capacity) || set.contains(buses[i])){
+                continue;
+            }
+            if(buses[i]>ans){
+                ans = buses[i];
+                break;
+            }
+        }
+        return ans;
+    }
+}
