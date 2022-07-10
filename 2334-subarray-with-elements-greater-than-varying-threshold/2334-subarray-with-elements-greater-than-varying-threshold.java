1dca4a1378329cc23c0f9a989e31161b3ab8743e
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-10 18:16:21 +0530

    Time: 274 ms (16.67%), Space: 92.9 MB (16.67%) - LeetHub

---
+++ 2334-subarray-with-elements-greater-than-varying-threshold/2334-subarray-with-elements-greater-than-varying-threshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int validSubarraySize(int[] nums, int threshold) {
+        int n = nums.length;
+        int len[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<n;i++){
+            if(st.isEmpty() || nums[st.peek()]<=nums[i]){
+                st.push(i);
+            }else{
+                while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                    int pop = st.pop();
+                    int low = st.isEmpty()?-1:st.peek();
+                    len[pop] = i-low-1;
+                }
+                st.push(i);
+            }
+        }
+        while(!st.isEmpty()){
+            int pop = st.pop();
+            int low = st.isEmpty()?-1:st.peek();
+            len[pop] = n-low-1;
+        }
+        /*for(int i=0;i<n;i++){
+            System.out.print(len[i]+" ");
+        }
+        System.out.println();*/
+        for(int i=0;i<n;i++){
+            if(nums[i]>threshold/len[i]){
+                return len[i];
+            }
+        }
+        return -1;
+    }
+}
