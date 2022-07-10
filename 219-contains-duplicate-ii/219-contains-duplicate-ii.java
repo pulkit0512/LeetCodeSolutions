7a0f14a3d83b4795981157a905d9e2059acade4c
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-11 00:29:49 +0530

    Time: 35 ms (67.03%), Space: 76.3 MB (77.30%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -16,6 +16,26 @@ class Solution {
         }
     }
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        //return containsNearbyDuplicateNLogN(nums, k);
+        return containsNearbyDuplicateN(nums, k);
+    }
+    
+    private boolean containsNearbyDuplicateN(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+            if(set.size()>k){
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+    
+    private boolean containsNearbyDuplicateNLogN(int[] nums, int k) {
         int n = nums.length;
         Pair[] pArr = new Pair[n];
         for(int i=0;i<n;i++){
