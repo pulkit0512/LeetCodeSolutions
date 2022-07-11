8135121c72621268e711b2beec2211f23ba5cfb5
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-12 02:23:27 +0530

    Time: 9 ms (89.59%), Space: 70.2 MB (72.38%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -2,51 +2,46 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
+        int INT_MAX = 1000000;
         int dp[][] = new int[n][m];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], 1000000);
-        }
-        if(mat[0][0] == 0){
-            dp[0][0] = 0;
-        }
-        for(int i=1;i<m;i++){
-            if(mat[0][i]!=0){
-                dp[0][i] = Math.min(dp[0][i], dp[0][i-1] + 1);
-            }else{
-                dp[0][i] = 0;
-            }
-        }
-        for(int i=1;i<n;i++){
-            if(mat[i][0]!=0){
-                dp[i][0] = Math.min(dp[i][0], dp[i-1][0] + 1);
-            }else{
-                dp[i][0] = 0;
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j]!=0){
-                    dp[i][j] = Math.min(dp[i][j], Math.min(dp[i-1][j], dp[i][j-1]) + 1);
-                }else{
-                    dp[i][j] = 0;
+                    dp[i][j] = INT_MAX;
                 }
             }
         }
         
-        for(int i=m-2;i>=0;i--){
-            if(mat[n-1][i]!=0){
-                dp[n-1][i] = Math.min(dp[n-1][i], dp[n-1][i+1] + 1);
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(mat[i][m-1]!=0){
-                dp[i][m-1] = Math.min(dp[i][m-1], dp[i+1][m-1] + 1);
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=m-2;j>=0;j--){
+        // top to bottom, left to right. This will take care of [i][j-1] and [i-1][j] neighbours.
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j]!=0){
-                    dp[i][j] = Math.min(dp[i][j], Math.min(dp[i+1][j], dp[i][j+1]) + 1);
+                    if(i==0 && j==0){
+                        continue;
+                    }else if(i==0){
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j-1]+1);
+                    }else if(j==0){
+                        dp[i][j] = Math.min(dp[i][j], dp[i-1][j]+1);
+                    }else{
+                        dp[i][j] = Math.min(dp[i][j], Math.min(dp[i-1][j], dp[i][j-1]) + 1);
+                    }
+                }
+            }
+        }
+        
+        // bottom to top, right to left. This will take care of [i][j+1] and [i+1][j] neighbours.
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(mat[i][j]!=0){
+                    if(i==n-1 && j==m-1){
+                        continue;
+                    }else if(i==n-1){
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j+1]+1);
+                    }else if(j==m-1){
+                        dp[i][j] = Math.min(dp[i][j], dp[i+1][j]+1);
+                    }else{
+                        dp[i][j] = Math.min(dp[i][j], Math.min(dp[i+1][j], dp[i][j+1]) + 1);
+                    }
                 }
             }
         }
