5b76319f9bc04c403dffd2c3ac376ff0b9b345d9
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-14 01:19:25 +0530

    Time: 13 ms (21.18%), Space: 45.3 MB (9.30%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,38 @@
+class Solution {
+    String plus = "+";
+    String minus = "-";
+    String mul = "*";
+    String div = "/";
+    public int evalRPN(String[] tokens) {
+        int n = tokens.length;
+        Stack<String> st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty() || isOperation(tokens[i])){
+                st.push(tokens[i]);
+            }else if(isOperation(st.peek())){
+                st.push(tokens[i]);
+            }else{
+                int a = Integer.valueOf(tokens[i]);
+                while(!st.isEmpty() && !isOperation(st.peek())){
+                    int b = Integer.valueOf(st.pop());
+                    String opr = st.pop();
+                    if(opr.equals(plus)){
+                        a += b;
+                    }else if(opr.equals(minus)){
+                        a -= b;
+                    }else if(opr.equals(mul)){
+                        a *= b;
+                    }else{
+                        a /= b;
+                    }
+                }
+                st.push(String.valueOf(a));
+            }
+        }
+        return Integer.valueOf(st.pop());
+    }
+    
+    private boolean isOperation(String s){
+        return s.equals(plus) || s.equals(minus) || s.equals(mul) || s.equals(div);
+    }
+}
