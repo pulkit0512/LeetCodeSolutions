71220d63353b824d44ad115b472e43957cd1809f
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-15 00:15:06 +0530

    Time: 30 ms (49.74%), Space: 45.3 MB (43.44%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,9 +2,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
         int dp[] = new int[amount+1];
-        for(int i=1;i<=amount;i++){
-            dp[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for(int i=0;i<coins.length;i++){
             for(int j=coins[i];j<=amount;j++){
                 if(dp[j-coins[i]]!=Integer.MAX_VALUE){
@@ -12,9 +11,6 @@ class Solution {
                 }
             }
         }
-        if(dp[amount]==Integer.MAX_VALUE){
-            return -1;
-        }
-        return dp[amount];
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }
