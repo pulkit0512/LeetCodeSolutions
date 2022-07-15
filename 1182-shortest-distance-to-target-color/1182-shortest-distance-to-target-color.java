bf697838de7336fe90bc307372d53801776d4d31
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-16 00:35:59 +0530

    Time: 20 ms (92.95%), Space: 126.2 MB (44.66%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.java
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.java
@@ -7,43 +7,36 @@ class Solution {
     private List<Integer> shortestDistanceColorNPlusQ(int[] colors, int[][] queries) {
         List<Integer> result = new ArrayList<>();
         int n = colors.length;
-        int left[][] = new int[3][n];
-        int right[][] = new int[3][n];
+        int dist[][] = new int[3][n];
         int one = -1, two = -1, three = -1;
         for(int i=0;i<n;i++){
             if(colors[i]==1){
-                left[0][i] = i;
-                left[1][i] = two;
-                left[2][i] = three;
+                dist[1][i] = (two==-1)?Integer.MAX_VALUE:i-two;
+                dist[2][i] = (three==-1)?Integer.MAX_VALUE:i-three;
                 one = i;
             }else if(colors[i]==2){
-                left[0][i] = one;
-                left[1][i] = i;
-                left[2][i] = three;
+                dist[0][i] = (one==-1)?Integer.MAX_VALUE:i-one;
+                dist[2][i] = (three==-1)?Integer.MAX_VALUE:i-three;
                 two = i;
             }else{
-                left[0][i] = one;
-                left[1][i] = two;
-                left[2][i] = i;
+                dist[0][i] = (one==-1)?Integer.MAX_VALUE:i-one;
+                dist[1][i] = (two==-1)?Integer.MAX_VALUE:i-two;
                 three = i;
             }
         }
         one = two = three = -1;
         for(int i=n-1;i>=0;i--){
             if(colors[i]==1){
-                right[0][i] = i;
-                right[1][i] = two;
-                right[2][i] = three;
+                dist[1][i] = Math.min(dist[1][i], (two==-1)?Integer.MAX_VALUE:two-i);
+                dist[2][i] = Math.min(dist[2][i], (three==-1)?Integer.MAX_VALUE:three-i);
                 one = i;
             }else if(colors[i]==2){
-                right[0][i] = one;
-                right[1][i] = i;
-                right[2][i] = three;
+                dist[0][i] = Math.min(dist[0][i], (one==-1)?Integer.MAX_VALUE:one-i);
+                dist[2][i] = Math.min(dist[2][i], (three==-1)?Integer.MAX_VALUE:three-i);
                 two = i;
             }else{
-                right[0][i] = one;
-                right[1][i] = two;
-                right[2][i] = i;
+                dist[0][i] = Math.min(dist[0][i], (one==-1)?Integer.MAX_VALUE:one-i);
+                dist[1][i] = Math.min(dist[1][i], (two==-1)?Integer.MAX_VALUE:two-i);
                 three = i;
             }
         }
@@ -51,17 +44,8 @@ class Solution {
         for(int[] query:queries){
             int idx = query[0];
             int color = query[1]-1;
-            int lower = left[color][idx];
-            int upper = right[color][idx];
-            if(lower==-1 && upper==-1){
-                result.add(-1);
-            }else if(lower==-1 && upper!=-1){
-                result.add(upper-idx);
-            }else if(lower!=-1 && upper==-1){
-                result.add(idx-lower);
-            }else{
-                result.add(Math.min(upper-idx, idx-lower));
-            }
+            int val = (dist[color][idx]==Integer.MAX_VALUE)?-1:dist[color][idx];
+            result.add(val);
         }
         return result;
     }
