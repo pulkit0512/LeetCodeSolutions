c110724c96a7672fd27256d37cb672ba6ae99ec3
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-17 00:00:18 +0530

    Time: 13 ms (16.10%), Space: 50.4 MB (8.86%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>[] dp = new ArrayList[target+1];
+        for(int i=0;i<=target;i++){
+            dp[i] = new ArrayList<>();
+        }
+        dp[0].add(new ArrayList<>());
+        Arrays.sort(candidates);
+        for(int i=0;i<candidates.length;i++){
+            for(int j=candidates[i];j<=target;j++){
+                for(List<Integer> list:dp[j-candidates[i]]){
+                    dp[j].add(new ArrayList<>(list));
+                    dp[j].get(dp[j].size()-1).add(candidates[i]);
+                }
+            }
+        }
+        /*for(int i=0;i<=target;i++){
+            System.out.println(dp[i]);
+        }*/
+        return dp[target];
+    }
+}
