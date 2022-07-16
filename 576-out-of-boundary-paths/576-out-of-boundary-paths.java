cff7aab1a84bc3dde64e83484a558552b7f254e3
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-16 20:40:33 +0530

    Time: 23 ms (16.04%), Space: 43.2 MB (33.75%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -3,6 +3,54 @@ class Solution {
     Integer dp[][][];
     int dir[][] = new int[][]{{0,-1},{1,0},{0,1},{-1,0}};
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        //return findPathsMemoization(m, n, maxMove, startRow, startColumn);
+        return findPathsTabulation(m, n, maxMove, startRow, startColumn);
+    }
+    
+    private int findPathsTabulation(int m, int n, int maxMove, int startRow, int startColumn) {
+        int dp[][] = new int[m][n];
+        dp[startRow][startColumn] = 1;
+        int paths = 0;
+        for(int move=1;move<=maxMove;move++){
+            int temp[][] = new int[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i>=0){
+                        if(i==0){
+                            paths = (paths + dp[i][j])%mod;
+                        }else{
+                            temp[i][j] = dp[i-1][j];
+                        }
+                    }
+                    if(j>=0){
+                        if(j==0){
+                            paths = (paths + dp[i][j])%mod;
+                        }else{
+                            temp[i][j] = (temp[i][j] + dp[i][j-1])%mod;
+                        }
+                    }
+                    if(i<=m-1){
+                        if(i==m-1){
+                            paths = (paths + dp[i][j])%mod;
+                        }else{
+                            temp[i][j] = (temp[i][j] + dp[i+1][j])%mod;
+                        }
+                    }
+                    if(j<=n-1){
+                        if(j==n-1){
+                            paths = (paths + dp[i][j])%mod;
+                        }else{
+                            temp[i][j] = (temp[i][j] + dp[i][j+1])%mod;
+                        }
+                    }
+                }
+            }
+            dp = temp;
+        }
+        return paths;
+    }
+    
+    private int findPathsMemoization(int m, int n, int maxMove, int startRow, int startColumn) {
         dp = new Integer[m][n][maxMove+1];
         dp[startRow][startColumn][0] = 0;
         findPathsUtil(m, n, maxMove, startRow, startColumn);
