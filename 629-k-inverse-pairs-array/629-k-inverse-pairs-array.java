175f2e08c5ff84e98623f3410b4cdef7777a6e3c
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-18 03:31:42 +0530

    Time: 33 ms (62.65%), Space: 47.5 MB (72.29%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -1,7 +1,8 @@
 class Solution {
     int mod = 1000000007;
     public int kInversePairs(int n, int k) {
-        return kInversePairsUsingCumulativeSumN2Space(n, k);
+        //return kInversePairsUsingCumulativeSumN2Space(n, k);
+        return kInversePairsUsingCumulativeSumNSpace(n, k);
     }
     
     private int kInversePairsUsingCumulativeSumN2Space(int n, int k) {
@@ -26,6 +27,34 @@ class Solution {
         int ans = dp[n][k];
         if(k-1>=0){
             ans = ans-dp[n][k-1];
+            if(ans<0){
+                ans = ans + mod;
+            }
+        }
+        return ans%mod;
+    }
+    
+    private int kInversePairsUsingCumulativeSumNSpace(int n, int k){
+        int dpPrev[] = new int[k+1];
+        for(int i=1;i<=n;i++){
+            int dpCurr[] = new int[k+1];
+            dpCurr[0] = 1;
+            for(int j=1;j<=k;j++){
+                int val = dpPrev[j];
+                if(j-i>=0){
+                    val = val - dpPrev[j-i];
+                    if(val<0){
+                        val = val + mod;
+                    }
+                }
+                val = val%mod;
+                dpCurr[j] = (dpCurr[j-1] + val)%mod;
+            }
+            dpPrev = dpCurr;
+        }
+        int ans = dpPrev[k];
+        if(k-1>=0){
+            ans = ans-dpPrev[k-1];
             if(ans<0){
                 ans = ans + mod;
             }
