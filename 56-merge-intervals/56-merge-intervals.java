f984fb677cccbe8bffce15b5e8cf3d0d32e06d87
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-17 15:55:39 +0530

    Time: 21 ms (12.59%), Space: 54.9 MB (74.95%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        Arrays.sort(intervals, (a,b)->(a[0]-b[0]));
         Stack<int[]> st = new Stack<>();
         for(int[] interval:intervals){
-            if(st.isEmpty()){
+            if(st.isEmpty() || st.peek()[1]<interval[0]){
                 st.push(interval);
-            }else if(interval[0]<=st.peek()[1]){
-                if(interval[1]>st.peek()[1]){
+            }else{
+                if(st.peek()[1]<interval[1]){
                     st.peek()[1] = interval[1];
                 }
-            }else{
-                st.push(interval);
             }
         }
         int[][] ans = new int[st.size()][2];
-        for(int i=st.size()-1;i>=0;i--){
+        int size = st.size();
+        for(int i=size-1;i>=0;i--){
             ans[i] = st.pop();
         }
         return ans;
