c98e958b45152fc2df0bb0bf2b79fa918ccb59bd
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-19 06:26:59 +0530

    Time: 130 ms (94.04%), Space: 43.3 MB (88.83%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,29 +1,27 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int row = matrix.length, col = matrix[0].length;
-        // compute 2D prefix sum
-        int[][] preSum = new int[row + 1][col + 1];
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
-                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] - preSum[i - 1][j - 1] + matrix[i - 1][j - 1];
+        int preSum[][] = new int[row+1][col+1];
+        //Compute 2D prefix Sum
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + matrix[i-1][j-1];
             }
         }
-        int count = 0, currSum;
-        Map<Integer, Integer> map = new HashMap();
-        // reduce 2D problem to 1D one
-        // by fixing two rows r1 and r2 and 
-        // computing 1D prefix sum for all matrices using [r1..r2] rows
-        for (int r1 = 1; r1 <= row; r1++) {
-            for (int r2 = r1; r2 <= row; r2++) {
+        // Map to store subarray counts for a particular sum
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int r1 = 1;r1 <= row;r1++){
+            for(int r2=r1;r2 <= row;r2++){
                 map.clear();
-                map.put(0, 1);
-                for (int c = 1; c <= col; c++) {
-                    // current 1D prefix sum
-                    currSum = preSum[r2][c] - preSum[r1 - 1][c];
-                    // add subarrays which sum up to (currSum - target)
-                    count += map.getOrDefault(currSum - target, 0);
-                    // save current prefix sum
-                    map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+                map.put(0,1);
+                for(int c=1;c<=col;c++){
+                    //Compute 1D Subarray sum for particular column between two rows.
+                    int curSum = preSum[r2][c] - preSum[r1-1][c];
+                    // add the number of subarrays to the count whose sum equal to curSum-target
+                    count += map.getOrDefault(curSum-target, 0);
+                    // increse frequency of curSum.
+                    map.put(curSum, map.getOrDefault(curSum, 0) + 1);
                 }
             }
         }
