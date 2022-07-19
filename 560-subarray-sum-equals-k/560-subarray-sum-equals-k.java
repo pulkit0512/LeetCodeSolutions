925e699df4239a65391f31da6ea6e377a30f9f1e
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-19 06:37:18 +0530

    Time: 22 ms (92.27%), Space: 46 MB (92.67%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,19 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
         int sum = 0;
-        map.put(sum, 1);
-        int ans = 0;
-        for(int i=0;i<nums.length;i++){
-            sum += nums[i];
-            if(map.containsKey(sum-k)){
-                ans += map.get(sum-k);
-            }
-            if(!map.containsKey(sum)){
-                map.put(sum, 0);
-            }
-            map.put(sum, map.get(sum)+1);
+        int count = 0;
+        for(int num:nums){
+            // Compute prefix sum upto index i.
+            sum += num;
+            // Add count of subarrays already in map with sum = curSum - k.
+            // This is done because, if for two indices i and j, sum[i]-sum[j] == k
+            // then sum of elements between i and j equals to k.
+            count += map.getOrDefault(sum-k, 0);
+            // increment frequency of curSum
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return ans;
+        return count;
     }
 }
