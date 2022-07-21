cf61275004545bfcb151e42b90f1a2150f6d6ea9
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-21 16:38:22 +0530

    Time: 234 ms (27.42%), Space: 41.3 MB (86.06%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,13 +1,13 @@
 class Solution {
-    int[][] dir = new int[][]{{0,-1},{1,0},{0,1},{-1,0}};
+    int dir[][] = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
     public boolean exist(char[][] board, String word) {
         int len = word.length();
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==word.charAt(0)){
+                if(board[i][j] == word.charAt(0)){
                     char temp = board[i][j];
-                    board[i][j] = ' ';
-                    if(wordSearchHelper(board, i, j, word, 1, len)){
+                    board[i][j] = '0';
+                    if(wordSearchUtil(board, word, 1, len, i, j)){
                         return true;
                     }
                     board[i][j] = temp;
@@ -16,18 +16,19 @@ class Solution {
         }
         return false;
     }
-    private boolean wordSearchHelper(char[][] board, int i, int j, String word, int idx, int len) {
+    
+    private boolean wordSearchUtil(char[][] board, String word, int idx, int len, int row, int col) {
         if(idx==len){
             return true;
         }
-        for(int[] d:dir){
-            int newRow = d[0]+i;
-            int newCol = d[1]+j;
+        for(int i=0;i<4;i++){
+            int newRow = row + dir[i][0];
+            int newCol = col + dir[i][1];
             if(newRow>=0 && newCol>=0 && newRow<board.length && newCol<board[0].length 
                && board[newRow][newCol]==word.charAt(idx)){
                 char temp = board[newRow][newCol];
-                board[newRow][newCol] = ' ';
-                if(wordSearchHelper(board, newRow, newCol, word, idx+1, len)){
+                board[newRow][newCol] = '0';
+                if(wordSearchUtil(board, word, idx+1, len, newRow, newCol)){
                     return true;
                 }
                 board[newRow][newCol] = temp;
