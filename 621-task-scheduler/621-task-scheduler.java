f451007e630812e51e79f715f943789136d8011f
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-23 18:32:43 +0530

    Time: 3 ms (90.04%), Space: 59.8 MB (60.22%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,5 +1,27 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
+        //return leastIntervalPriorityQueue(tasks, n);
+        return leastIntervalSort(tasks, n);
+    }
+    
+    private int leastIntervalSort(char[] tasks, int n) {
+        int freq[] = new int[26];
+        for(char ch:tasks){
+            freq[ch-'A']++;
+        }
+        Arrays.sort(freq);
+        int fMax = freq[25];
+        // Max idle time can be (fMax-1)*n i.e. time needed to complete the maximum frequence task alone with cooling period
+        // of n.
+        int idle_time = (fMax-1)*n;
+        for(int i=24;i>=0;i--){
+            //See how many idle slots to can be filled with current task.
+            idle_time = idle_time - Math.min(fMax-1, freq[i]);
+        }
+        return tasks.length + Math.max(0, idle_time);
+    }
+    
+    private int leastIntervalPriorityQueue(char[] tasks, int n) {
         int[] freq = new int[26];
         for(char ch:tasks){
             freq[ch-'A']++;
