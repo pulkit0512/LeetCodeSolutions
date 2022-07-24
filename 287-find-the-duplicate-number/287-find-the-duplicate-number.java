c8ccc959c49caef17e65440fd9098239b2cbf80f
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-24 15:12:06 +0530

    Time: 5 ms (85.89%), Space: 74.7 MB (82.83%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,5 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //return findDuplicateCyclicSort(nums);
+        return findDuplicateNegativeMarking(nums);
+    }
+    
+    private int findDuplicateNegativeMarking(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int val = Math.abs(nums[i]);
+            if(nums[val]<0){
+                return val;
+            }
+            nums[val] = -nums[val];
+        }
+        return n;
+    }
+    
+    private int findDuplicateCyclicSort (int[] nums) {
         int n = nums.length;
         for(int i=0;i<n;i++){
             while(nums[i]!=nums[nums[i]-1]){
