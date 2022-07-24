b740bd84face632c6bc207e08f1d24260641c181
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-24 23:52:10 +0530

    Time: 13 ms (33.20%), Space: 57.7 MB (56.23%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //return searchMatrixMLogN(matrix, target);
+        return searchMatrixMPlusN(matrix, target);
+    }
+    
+    private boolean searchMatrixMPlusN(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int ptrRow = m-1, ptrCol = 0;
+        while(ptrRow>=0 && ptrCol<n){
+            if(matrix[ptrRow][ptrCol]>target){
+                ptrRow--;
+            }else if(matrix[ptrRow][ptrCol]<target){
+                ptrCol++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean searchMatrixMLogN(int[][] matrix, int target) {
         int m = matrix.length;
         for(int i=0;i<m;i++){
             int val = Arrays.binarySearch(matrix[i], target);
