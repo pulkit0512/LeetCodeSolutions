3fab7993f3ecac3f633fe2fa15d0a80ebd28663c
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-25 00:06:56 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (95.48%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //return searchMatrixMPlusLogN(matrix, target);
+        return searchMatrixMPlusN(matrix, target);
+    }
+    
+    private boolean searchMatrixMPlusN(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int ptrRow = m-1, ptrCol = 0;
+        while(ptrRow>=0 && ptrCol<n){
+            if(matrix[ptrRow][ptrCol]>target){
+                ptrRow--;
+            }else if(matrix[ptrRow][ptrCol]<target){
+                ptrCol++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean searchMatrixMPlusLogN(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
         int row = 0;
