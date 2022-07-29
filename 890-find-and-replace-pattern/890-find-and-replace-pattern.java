de482af64fdba942a42c44c5f59bbe2ec07c506e
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-30 01:33:18 +0530

    Time: 1 ms (98.01%), Space: 42.9 MB (64.12%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -2,26 +2,27 @@ class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> result = new ArrayList<>();
         int pLen = pattern.length();
-        char[] mapping = new char[26];
+        char[] patMap = new char[26];
+        char[] wordMap = new char[26];
         for(String word:words) {
-            Arrays.fill(mapping, '0');
+            for(int i=0;i<26;i++){
+                patMap[i] = '0';
+                wordMap[i] = '0';
+            }
             boolean match = true;
             for(int i=0;i<pLen;i++){
-                int idx = pattern.charAt(i)-'a';
-                if(mapping[idx]=='0'){
-                    mapping[idx] = word.charAt(i);
-                }else if(mapping[idx] != word.charAt(i)){
+                int pIdx = pattern.charAt(i)-'a';
+                int wIdx = word.charAt(i)-'a';
+                if(patMap[pIdx]=='0'){
+                    patMap[pIdx] = word.charAt(i);
+                }else if(patMap[pIdx] != word.charAt(i)){
                     match = false;
                     break;
                 }
-            }
-            int freq[] = new int[26];
-            for(int i=0;i<26;i++){
-                if(mapping[i]=='0'){
-                    continue;
-                }
-                freq[mapping[i]-'a']++;
-                if(freq[mapping[i]-'a']>1){
+                
+                if(wordMap[wIdx] == '0'){
+                    wordMap[wIdx] = pattern.charAt(i);
+                }else if(wordMap[wIdx] != pattern.charAt(i)){
                     match = false;
                     break;
                 }
