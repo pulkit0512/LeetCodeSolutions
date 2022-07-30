2d66ef9434ea9870b552480d7c1d1f484cfee9be
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-30 21:10:08 +0530

    Time: 8 ms (88.33%), Space: 44.5 MB (48.05%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,9 +1,44 @@
 class Solution {
     public int calculate(String s) {
-        return calculateUsing1Stack(s);
+        //return calculateUsingOneStack1(s);
+        return calculateUsingOneStack2(s);
     }
     
-    private int calculateUsing1Stack(String s) {
+    private int calculateUsingOneStack2(String s) {
+        s = "(" + s + ")";
+        int len = s.length();
+        Stack<Integer> st = new Stack<>();
+        int result = 0, current = 0;
+        int sign = 1;
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
+            if(ch=='(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }else if(ch=='+' || ch=='-'){
+                result += current*sign;
+                current = 0;
+                sign = (ch=='+')?1:-1;
+            }else if(ch==' '){
+                continue;
+            }else if(ch==')'){
+                result += current*sign;
+                result *= st.pop();
+                result += st.pop();
+                current = 0;
+            }else {
+                current = current*10 + (ch-'0');
+            }
+        }
+        return result;
+    }
+    
+    // Storing each value in stack and when reached to ')'
+    // calculating the result untill we reach to '('
+    // Used integer extreme values for '(' in stack
+    private int calculateUsingOneStack1(String s) {
         s = "(" + s + ")";
         Stack<Integer> st = new Stack<>();
         int len = s.length();
