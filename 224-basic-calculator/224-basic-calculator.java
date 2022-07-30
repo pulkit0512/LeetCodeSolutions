833119e382bacd097375061fbb31ed0ab264c944
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-30 20:21:48 +0530

    Time: 27 ms (26.25%), Space: 45.7 MB (25.57%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,76 +1,48 @@
 class Solution {
     public int calculate(String s) {
-        //return calculateUsingTwoStacks(s);
-        return calculateUsingOneStack(s);
-    }
-    private int calculateUsingOneStack(String s) {
-        s = "("+s+")";
-        int len = s.length();
-        int result = 0, sign = 1;
-        int current = 0;
-        Stack<Integer> st = new Stack<>();
-        for(int i=0;i<len;i++){
-            char ch = s.charAt(i);
-            if(ch=='('){
-                st.push(result);
-                st.push(sign);
-                result = 0;
-                sign = 1;
-            }else if(ch=='+'){
-                result += current*sign;
-                sign = 1;
-                current = 0;
-            }else if(ch=='-'){
-                result += current*sign;
-                sign = -1;
-                current = 0;
-            }else if(ch ==' '){
-                continue;
-            }else if(Character.isDigit(ch)){
-                current = current*10 + (ch-'0');
-            }else{
-                result += current*sign;
-                result *= st.pop();
-                result += st.pop();
-                current = 0;
-            }
-        }
-        return result;
+        return calculateUsing1Stack(s);
     }
     
-    private int calculateUsingTwoStacks(String s) {
-        s = "("+s+")";
+    private int calculateUsing1Stack(String s) {
+        s = "(" + s + ")";
+        Stack<Integer> st = new Stack<>();
         int len = s.length();
-        Stack<Integer> values = new Stack<>();
-        Stack<Character> opr = new Stack<>();
         int val = 0;
+        int sign = 1;
         for(int i=0;i<len;i++){
-            //System.out.println(values+" "+opr);
             char ch = s.charAt(i);
             if(ch=='('){
-                opr.push(ch);
-            }else if(Character.isDigit(ch)){
-                val = val*10 + (ch-'0');
+                if(sign==-1){
+                    st.push(Integer.MIN_VALUE);
+                }else{
+                    st.push(Integer.MAX_VALUE);
+                }
+                sign = 1;
             }else if(ch==' '){
                 continue;
             }else if(ch=='+' || ch=='-'){
-                if(opr.peek()=='-'){
-                    values.push(-val);
-                }else{
-                    values.push(val);
+                if(val!=0){
+                    st.push(val*sign);
                 }
-                opr.push(ch);
+                sign = (ch=='+')?1:-1;
                 val = 0;
-            }else{
-                val = (opr.peek()=='-')?-val:val;
-                while(opr.peek()!='('){
-                    val += values.pop();
-                    opr.pop();
+            }else if(ch==')'){
+                val = val*sign;
+                while(st.peek()!=Integer.MIN_VALUE && st.peek()!=Integer.MAX_VALUE) {
+                    val += st.pop();
                 }
-                opr.pop();
+                int top = st.pop();
+                if(top==Integer.MIN_VALUE){
+                    st.push(-val);
+                }else{
+                    st.push(val);
+                }
+                val = 0;
+                sign = 1;
+            }else{
+                val = val*10 + (ch-'0');
             }
         }
-        
-        return val;
+        return st.pop();
     }
 }
