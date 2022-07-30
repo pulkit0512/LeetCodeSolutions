790af18a4a20dcb1e710e254277f07b28cdddf9b
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-31 00:25:56 +0530

    Time: 312 ms (27.37%), Space: 103.7 MB (8.30%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] nsLeft = new int[n];
+        int[] nsRight = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<n;i++){
+            if(st.isEmpty() || heights[i]>=heights[st.peek()]){
+                st.push(i);
+            }else{
+                while(!st.isEmpty() && heights[i]<heights[st.peek()]){
+                    nsRight[st.pop()] = i;
+                }
+                st.push(i);
+            }
+        }
+        while(!st.isEmpty()){
+            nsRight[st.pop()] = n;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            if(st.isEmpty() || heights[i]>=heights[st.peek()]){
+                st.push(i);
+            }else{
+                while(!st.isEmpty() && heights[i]<heights[st.peek()]){
+                    nsLeft[st.pop()] = i;
+                }
+                st.push(i);
+            }
+        }
+        while(!st.isEmpty()){
+            nsLeft[st.pop()] = -1;
+        }
+        /*for(int i=0;i<n;i++){
+            System.out.println(nsLeft[i] + " " + nsRight[i]);
+        }*/
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = Math.max(ans, (nsRight[i] - nsLeft[i] - 1)*heights[i]);
+        }
+        
+        return ans;
+    }
+}
