dcef0f477b1e76aa05b6f9c808d6ac1b73839e7d
Pulkit Arora <arorapulkit2@gmail.com>
2022-07-31 20:05:03 +0530

    Time: 40 ms (78.90%), Space: 72.7 MB (53.50%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -20,15 +20,21 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i] = new JobData(startTime[i], endTime[i], profit[i]);
         }
+        // Sort jobs on start time.
         Arrays.sort(arr, (a, b) -> (a.sTime - b.sTime));
         
         for(int i=n-1;i>=0;i--){
             int nextIndex = getNextJobIndex(arr, arr[i].eTime);
             int curProfit = arr[i].profit;
+            
+            // Find the next Job in right which can follow current job.
+            // current job end time<=next job start time
             if(nextIndex!=n){
                 curProfit += dp[nextIndex];
             }
             
+            // If by schedulling current job profit will increase then only schedule.
+            // Else ignore this job.
             if(i==n-1){
                 dp[i] = curProfit;
             }else{
@@ -60,20 +66,29 @@ class Solution {
         for(int i=0;i<n;i++){
             arr[i] = new JobData(startTime[i], endTime[i], profit[i]);
         }
+        //sort jobs on start time.
         Arrays.sort(arr, (a, b) -> (a.sTime - b.sTime));
         int maxProfit = 0;
+        // Have a priority queue which will have end of all conflicting jobs list with maximum profit.
         PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) -> (a.get(0) - b.get(0)));
         for(int i=0;i<n;i++){
+            //For current job remove all non conflicting jobs from priority queue
+            // remove all job whose end time in priority queue is >= current job start time.
+            // keep track of max profit from all these jobs.
             while(!pq.isEmpty() && arr[i].sTime>=pq.peek().get(0)){
                 maxProfit = Math.max(maxProfit, pq.poll().get(1));
             }
+            
+            // Add this current job to max profit job list.
             List<Integer> combined = new ArrayList<>();
             combined.add(arr[i].eTime);
             combined.add(arr[i].profit + maxProfit);
             
+            // push combined job list back to priority queue.
             pq.add(combined);
         }
         
+        // get the maximum profit from all the conflicting job lists.
         while(!pq.isEmpty()){
             maxProfit = Math.max(maxProfit, pq.poll().get(1));
         }
