797e571c56f9a1e7c811adf80ea11cd6a28fba8f
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-01 14:29:25 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (98.27%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,13 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         if(m<n){
-            return uniquePaths(n,m);
+            return uniquePaths(n, m);
         }
         int dp[] = new int[n];
         Arrays.fill(dp, 1);
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                dp[j] = dp[j] + dp[j-1];
+                dp[j] += dp[j-1];
             }
         }
         return dp[n-1];
