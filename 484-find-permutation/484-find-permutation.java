c412ed3c8689620b5f3a012b4e9590629df03088
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-01 14:55:43 +0530

    Time: 7 ms (47.87%), Space: 44 MB (100.00%) - LeetHub

--- 484-find-permutation/484-find-permutation.java
+++ 484-find-permutation/484-find-permutation.java
@@ -1,5 +1,33 @@
 class Solution {
     public int[] findPermutation(String s) {
+        //return findPermutationUsingTwoPointers(s);
+        return findPermutationUsingStack(s);
+    }
+    
+    private int[] findPermutationUsingStack(String s) {
+        int len = s.length();
+        Stack<Integer> st = new Stack<>();
+        int res[] = new int[len+1];
+        int j = 0;
+        for(int i=1;i<=len;i++){
+            char ch = s.charAt(i-1);
+            if(ch=='I'){
+                res[j++] = i;
+                while(!st.isEmpty()){
+                    res[j++] = st.pop();
+                }
+            }else{
+                st.push(i);
+            }
+        }
+        res[j++] = len+1;
+        while(!st.isEmpty()){
+            res[j++] = st.pop();
+        }
+        return res;
+    }
+    
+    private int[] findPermutationUsingTwoPointers(String s) {
         int len = s.length();
         int[] perm = new int[len+1];
         int val = 1;
