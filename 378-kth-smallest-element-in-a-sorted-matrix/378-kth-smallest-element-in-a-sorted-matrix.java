e25ae340fd690ccda36ca381ab85c2bfe78bc9e9
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-02 17:46:30 +0530

    Time: 0 ms (100.00%), Space: 58.1 MB (8.12%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -7,7 +7,63 @@ class Solution {
             val = v;
         }
     }
+    
     public int kthSmallest(int[][] matrix, int k) {
+        //return kthSmallestUsingPriorityQueue(matrix, k);
+        return kthSmallestUsingBinarySearch(matrix, k);
+    }
+    
+    // Time Complexity: O(NLog(Max-Min))
+    // Space Complexity: O(1)
+    private int kthSmallestUsingBinarySearch(int[][] matrix, int k) {
+        int n = matrix.length;
+        int min = matrix[0][0];
+        int max = matrix[n-1][n-1];
+        while(min<max){
+            // get lower value of mid.
+            int mid = max+min;
+            if(mid<0){
+                mid = (mid-1)/2;
+            }else{
+                mid = mid/2;
+            }
+            int[] midData = getMidData(matrix, mid);
+            if(midData[0] == k){
+                return midData[1];
+            }else if(midData[0]>k){
+                max = midData[1];
+            }else{
+                min = midData[2];
+            }
+        }
+        return min;
+    }
+    
+    // midData[0] = count of elements less then equal to mid
+    // midData[1] = largest element smaller then mid
+    // midData[2] = smallest element larget then mid
+    private int[] getMidData(int[][] matrix, int mid) {
+        int[] midData = new int[3];
+        int n = matrix.length;
+        midData[1] = matrix[0][0];
+        midData[2] = matrix[n-1][n-1];
+        int i = n-1, j = 0;
+        while(i>=0 && j<n) {
+            if(matrix[i][j]<=mid){
+                midData[1] = Math.max(midData[1], matrix[i][j]);
+                midData[0] += i+1;
+                j++;
+            }else{
+                midData[2] = Math.min(midData[2], matrix[i][j]);
+                i--;
+            }
+        }
+        return midData;
+    }
+    
+    // Time Complexity: O(Min(K, N) + KLog(Min(k, N)))
+    // Space Complexity: O(Min(K, N))
+    private int kthSmallestUsingPriorityQueue(int[][] matrix, int k) {
         PriorityQueue<Data> pq = new PriorityQueue<>((a,b) -> (a.val-b.val));
         int minRows = Math.min(k, matrix.length);
         for(int i=0;i<minRows;i++){
