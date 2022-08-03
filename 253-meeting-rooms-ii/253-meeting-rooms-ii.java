0ef870f0a8d2ffe04ed63f4f173317154453f6b3
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-03 14:54:36 +0530

    Time: 12 ms (47.05%), Space: 44.9 MB (68.22%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    class sort implements Comparator<int[]>{
+        public int compare(int[] a, int[] b){
+            if(a[0]==b[0]){
+                return a[1]-b[1];
+            }
+            return a[0]-b[0];
+        }
+    }
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, new sort());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<intervals.length;i++){
+            if(!pq.isEmpty() && intervals[i][0]>=pq.peek()){
+                pq.poll();
+            }
+            
+            pq.add(intervals[i][1]);
+        }
+        return pq.size();
+    }
+}
