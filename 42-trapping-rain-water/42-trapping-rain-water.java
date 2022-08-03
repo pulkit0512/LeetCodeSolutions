68a2c33c79d160e984695167efa7db57ced48a9a
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-03 13:49:55 +0530

    Time: 2 ms (59.38%), Space: 48.1 MB (80.36%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,5 +1,36 @@
 class Solution {
     public int trap(int[] height) {
+        //return trapNSpace(height);
+        return trapConstantSpace(height);
+    }
+    
+    // O(1) Space using extreme pointers
+    private int trapConstantSpace(int[] height) {
+        int left = 0, right = height.length-1;
+        int ans = 0;
+        int left_max = 0, right_max = 0;
+        while(left<=right){
+            if(height[left]<height[right]) {
+                if(height[left]>=left_max){
+                    left_max = height[left];
+                }else{
+                    ans += left_max - height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=right_max){
+                    right_max = height[right];
+                }else{
+                    ans += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+    
+    // O(N) Space
+    private int trapNSpace(int[] height) {
         int n = height.length;
         int left[] = new int[n];
         int right[] = new int[n];
