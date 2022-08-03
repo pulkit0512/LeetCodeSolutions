9e4a83a7842fef60935b0daf11b604dd62f2a905
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-03 20:26:53 +0530

    Time: 1 ms (88.87%), Space: 42.1 MB (58.79%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,5 +1,39 @@
 class Solution {
     public String decodeString(String s) {
+        //return decodeStringSingleStack(s);
+        return decodeStringTwoStacks(s);
+    }
+    
+    private String decodeStringTwoStacks(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        int len = s.length();
+        int val = 0;
+        StringBuilder curStr = new StringBuilder();
+        for(int i=0;i<len;i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                val = val*10 + (ch-'0');
+            }else if(ch=='['){
+                countStack.push(val);
+                stringStack.push(curStr);
+                val = 0;
+                curStr = new StringBuilder();
+            }else if(ch==']'){
+                int count = countStack.pop();
+                StringBuilder decodedString = stringStack.pop();
+                for(int j=count;j>0;j--){
+                    decodedString.append(curStr);
+                }
+                curStr = decodedString;
+            }else{
+                curStr.append(ch);
+            }
+        }
+        return curStr.toString();
+    }
+    
+    private String decodeStringSingleStack(String s) {
         s = s+"]";
         Stack<Object> st = new Stack<>();
         char open = '[';
