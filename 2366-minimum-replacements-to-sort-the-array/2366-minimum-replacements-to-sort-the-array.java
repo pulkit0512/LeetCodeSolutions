90ab044167370f5ffdd03dae040329464e96e56b
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-06 21:47:40 +0530

    Time: 11 ms (14.29%), Space: 80.8 MB (57.14%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumReplacement(int[] nums) {
+        long ans = 0;
+        int n = nums.length;
+        int min = nums[n-1];
+        for(int i=n-1;i>=0;i--){
+            int val = (int)Math.ceil(nums[i]/(1.0*min));
+            ans = ans + val - 1;
+            if(min>(nums[i]/val)){
+                min = nums[i]/val;
+            }
+        }
+        return ans;
+    }
+}
