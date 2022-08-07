0e3926173bcb2b58680282efca4774664dce7fad
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-07 14:46:33 +0530

    Time: 26 ms (55.78%), Space: 49.1 MB (50.30%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+        int[] vowels = new int[5];
+        int mod = 1000000007;
+        Arrays.fill(vowels, 1);
+        for(int i=2;i<=n;i++){
+            int[] cur = new int[5];
+            cur[0] = vowels[1];
+            cur[1] = (vowels[0] + vowels[2])%mod;
+            cur[2] = ((vowels[0] + vowels[1])%mod + (vowels[3] + vowels[4])%mod)%mod;
+            cur[3] = (vowels[2] + vowels[4])%mod;
+            cur[4] = vowels[0];
+            vowels = cur;
+        }
+        int ans = 0;
+        for(int i=0;i<5;i++){
+            ans = (ans + vowels[i])%mod;
+        }
+        return ans;
+    }
+}
