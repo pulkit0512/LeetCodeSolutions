27a76f857b084f7f9593fb30b47e29c4fa7123a5
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-07 20:08:53 +0530

    Time: 14 ms (13.47%), Space: 50.8 MB (8.29%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        //return combinationSumDP(candidates, target);
-        return combinationSumBacktrack(candidates, target);
+        return combinationSumDP(candidates, target);
     }
     
     private List<List<Integer>> combinationSumDP(int[] candidates, int target) {
@@ -10,6 +9,7 @@ class Solution {
             dp[i] = new ArrayList<>();
         }
         dp[0].add(new ArrayList<>());
+        
         for(int i=0;i<candidates.length;i++){
             for(int j=candidates[i];j<=target;j++){
                 for(List<Integer> list:dp[j-candidates[i]]){
@@ -18,31 +18,6 @@ class Solution {
                 }
             }
         }
-        /*for(int i=0;i<=target;i++){
-            System.out.println(dp[i]);
-        }*/
         return dp[target];
-    }
-    
-    List<List<Integer>> result;
-    private List<List<Integer>> combinationSumBacktrack(int[] candidates, int target) {
-        result = new ArrayList<>();
-        backtrack(candidates, target, new ArrayList<>(), 0);
-        return result;
-    }
-    
-    private void backtrack(int[] candidates, int target, List<Integer> list, int start) {
-        if(target==0){
-            result.add(new ArrayList<>(list));
-            return;
-        }
-        if(target<0){
-            return;
-        }
-        for(int i=start;i<candidates.length;i++){
-            list.add(candidates[i]);
-            backtrack(candidates, target-candidates[i], list, i);
-            list.remove(list.size()-1);
-        }
     }
 }
