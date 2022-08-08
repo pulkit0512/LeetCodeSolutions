165efcabd95bd85ef004860569c11280154d002e
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-09 00:02:39 +0530

    Time: 10 ms (97.31%), Space: 117.5 MB (79.45%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,5 +1,28 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        //return dailyTemperaturesNSpace(temperatures);
+        return dailyTemperaturesConstantSpace(temperatures);
+    }
+    
+    private int[] dailyTemperaturesConstantSpace(int[] temperatures) {
+        int hottest = 0;
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        for(int curDay=n-1;curDay>=0;curDay--){
+            if(temperatures[curDay]>=hottest){
+                hottest = temperatures[curDay];
+            }else{
+                int day = 1;
+                while(temperatures[curDay+day]<=temperatures[curDay]){
+                    day += ans[curDay+day];
+                }
+                ans[curDay] = day;
+            }
+        }
+        return ans;
+    }
+    
+    private int[] dailyTemperaturesNSpace(int[] temperatures) {
         Stack<Integer> st = new Stack<>();
         int n = temperatures.length;
         int ans[] = new int[n];
