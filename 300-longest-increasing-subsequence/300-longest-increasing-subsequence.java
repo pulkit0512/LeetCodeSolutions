bb283b8e95107c68e6c69703326fc75ae59e859b
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-08 23:27:08 +0530

    Time: 89 ms (38.31%), Space: 44.1 MB (69.04%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,47 +1,47 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        //return lengthOfLisN2(nums);
-        return lengthOfLisNLogN(nums);
-    }
-    
-    private int lengthOfLisN2(int[] nums) {
-        int n = nums.length;
-        int ans = 1;
-        int lis[] = new int[n];
-        lis[0] = 1;
-        for(int i=1;i<n;i++){
-            int max = 0;
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j]){
-                    max = Math.max(max, lis[j]);
-                }
-                if(max==ans){
-                    break;
-                }
-            }
-            lis[i] = max+1;
-            ans = Math.max(ans, lis[i]);
-        }
-        return ans;
+        return lengthOfLisN2(nums);
+        //return lengthOfLisNLogN(nums);
     }
     
     private int lengthOfLisNLogN(int[] nums) {
         int n = nums.length;
-		int tail[] = new int[n];
-		tail[0] = nums[0];
-		int length = 1;
-		for(int i=1;i<n;i++){
-			if(nums[i]>tail[length-1]){
-				tail[length] = nums[i];
-				length++;
-			} else {
-				int idx = Arrays.binarySearch(tail, 0, length-1, nums[i]);
-				if(idx<0){
-					idx = -(idx + 1);
-				}
-				tail[idx] = nums[i];
-			}
-		}
-        return length;
+        int lis[] = new int[n];
+        lis[0] = nums[0];
+        int len = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>lis[len-1]){
+                lis[len] = nums[i];
+                len++;
+            }else{
+                int idx = Arrays.binarySearch(lis, 0, len-1, nums[i]);
+                if(idx<0){
+                    idx = -(idx+1);
+                }
+                lis[idx] = nums[i];
+            }
+        }
+        return len;
+    }
+    
+    private int lengthOfLisN2(int[] nums) {
+        int n = nums.length;
+        int oMax = 1;
+        int dp[] = new int[n];
+        dp[0] = 1;
+        for(int i=1;i<n;i++){
+            int max = 0;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    max = Math.max(max, dp[j]);
+                }
+                if(max==oMax){
+                    break;
+                }
+            }
+            dp[i] = max+1;
+            oMax = Math.max(oMax, dp[i]);
+        }
+        return oMax;
     }
 }
