0e974d80d0dce54dd5eed499a54944d843d6bf76
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-10 00:04:59 +0530

    Time: 43 ms (72.83%), Space: 47.7 MB (35.84%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -2,6 +2,33 @@ class Solution {
     int mod = 1000000007;
     Map<Integer, Integer> dp;
     public int numFactoredBinaryTrees(int[] arr) {
+        //return numFactoredBinaryTreesMemoization(arr);
+        return numFactoredBinaryTreesTabulation(arr);
+    }
+    
+    private int numFactoredBinaryTreesTabulation(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        long[] ans = new long[n];
+        dp = new HashMap<>();
+        for(int i=0;i<n;i++){
+            ans[i] = 1;
+            dp.put(arr[i], i);
+        }
+        int totalTrees = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]%arr[j]==0 && dp.containsKey(arr[i]/arr[j])){
+                    int next = arr[i]/arr[j];
+                    ans[i] = (ans[i] + (ans[j]*ans[dp.get(next)])%mod)%mod;
+                }
+            }
+            totalTrees = (totalTrees + (int)ans[i])%mod;
+        }
+        return totalTrees;
+    }
+    
+    private int numFactoredBinaryTreesMemoization(int[] arr) {
         Map<Integer, Set<Integer>> factorMap = new HashMap<>();
         for(int x:arr){
             factorMap.put(x, new HashSet<>());
