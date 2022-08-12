23949999f755de4bca819156423b2f9ce297791b
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-12 22:16:44 +0530

    Time: 12 ms (58.97%), Space: 68.4 MB (16.16%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.java
@@ -0,0 +1,46 @@
+class Solution {
+    class CellData {
+        int row, col, steps;
+        public CellData(int r, int c, int s) {
+            row = r;
+            col = c;
+            steps = s;
+        }
+    }
+    int[][] dir = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    public int getFood(char[][] grid) {
+        Queue<CellData> que = new LinkedList<>();
+        int srcX = -1, srcY = -1;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='*'){
+                    srcX = i;
+                    srcY = j;
+                }
+            }
+        }
+        //System.out.println(srcX + " " + srcY);
+        //System.out.println(destX + " " + destY);
+        que.add(new CellData(srcX, srcY, 0));
+        while(!que.isEmpty()){
+            CellData cur = que.poll();
+            if(grid[cur.row][cur.col]=='X'){
+                continue;
+            }
+            //System.out.println(cur.row+" "+cur.col+" "+cur.steps);
+            if(grid[cur.row][cur.col]=='#'){
+                return cur.steps;
+            }
+            grid[cur.row][cur.col] = 'X';
+            for(int[] d:dir){
+                int newRow = cur.row + d[0];
+                int newCol = cur.col + d[1];
+                if(newRow>=0 && newCol>=0 && newRow<grid.length && newCol<grid[0].length
+                  && (grid[newRow][newCol]=='O' || grid[newRow][newCol]=='#')){
+                    que.add(new CellData(newRow, newCol, cur.steps+1));
+                }
+            }
+        }
+        return -1;
+    }
+}
