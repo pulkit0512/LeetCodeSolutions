7bbb91c5765a9031943e0dcfe1a230be2397c285
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-20 21:51:15 +0530

    Time: 2 ms (90.00%), Space: 42.1 MB (60.00%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -7,17 +7,20 @@ class Solution {
         }
         return ans;*/
         
+        // We need to move all the one's to the right.
         int len = s.length();
         int ans = 0;
         int cnt1 = 0;
         for(int i=0;i<len;i++){
             if(s.charAt(i)=='1') {
-                if(i==cnt1){
+                if(i==cnt1){ // If current 1 is on right possition continue.
                     cnt1++;
                     continue;
-                }else if(i-cnt1<=ans){
-                    ans++;
+                }else if(i-cnt1<=ans){ // If time needed to move current 1 to required position is less then
+                    ans++; // time we have spent then simply in next second it will reach it's position
                 }else{
+                    // Time needed for current 1 to reach it's required position will be equal to
+                    // current index - right index.
                     ans = i-cnt1;
                 }
                 cnt1++;
