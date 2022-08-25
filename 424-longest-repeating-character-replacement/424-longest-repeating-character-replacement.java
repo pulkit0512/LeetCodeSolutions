b47374748f51bb4a445beab7bc271fa2164b9cff
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-25 20:15:43 +0530

    Time: 14 ms (48.76%), Space: 42 MB (96.61%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -3,7 +3,14 @@ class Solution {
         int ans = 0;
         int len = s.length();
         int org_k = k;
+        int arr[] = new int[26];
+        for(int i=0;i<len;i++){
+            arr[s.charAt(i)-'A']++;
+        }
         for(char ch='A';ch<='Z';ch++){
+            if(arr[ch-'A']==0){
+                continue;
+            }
             int st = 0;
             k = org_k;
             for(int i=0;i<len;i++){
