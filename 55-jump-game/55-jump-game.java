e86e38878d68476371566cd7b51a79fc389292ab
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-26 02:27:46 +0530

    Time: 2 ms (93.29%), Space: 68.4 MB (32.62%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        if(n==1){
-            return true;
-        }
-        int reach = nums[0];
+        int reach = 0;
         for(int i=0;i<=reach;i++){
-            reach = Math.max(reach, nums[i]+i);
-            if(reach>=n-1){
+            reach = Math.max(reach, i+nums[i]);
+            if(reach>=nums.length-1){
                 return true;
             }
         }
