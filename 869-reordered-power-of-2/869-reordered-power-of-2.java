0781700f0b01fc4b221c7643575ccfb98da558dc
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-26 22:11:14 +0530

    Time: 11 ms (14.67%), Space: 42.1 MB (11.33%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        Map<Integer, Integer> orgMap = new HashMap<>();
+        Map<Integer, Integer> orgMap = new HashMap<>(10);
         while(n>0){
             orgMap.put(n%10, orgMap.getOrDefault(n%10, 0) + 1);
             n = n/10;
         }
         
-        //Set<Map<Integer, Integer>> mapSet = new HashSet<>(30);
         for(int i=0;i<30;i++){
             int val = (1<<i);
-            Map<Integer, Integer> map = new HashMap<>();
+            Map<Integer, Integer> map = new HashMap<>(10);
             while(val>0){
                 map.put(val%10, map.getOrDefault(val%10, 0) + 1);
                 val = val/10;
             }
+            
             if(orgMap.equals(map)){
                 return true;
             }
