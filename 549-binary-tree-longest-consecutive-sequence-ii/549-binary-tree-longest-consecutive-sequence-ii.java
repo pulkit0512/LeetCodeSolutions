1fa18e7e773877dc43a181cd9809c104cc0b6efc
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-29 01:13:02 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (89.55%) - LeetHub

---
+++ 549-binary-tree-longest-consecutive-sequence-ii/549-binary-tree-longest-consecutive-sequence-ii.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int longestPath;
+    public int longestConsecutive(TreeNode root) {
+        longestPath = 0;
+        longestConsecutiveUtil(root);
+        return longestPath;
+    }
+    
+    private int[] longestConsecutiveUtil(TreeNode root) {
+        if(root==null){
+            return new int[]{0, 0};
+        }
+        
+        int[] left = longestConsecutiveUtil(root.left);
+        int[] right = longestConsecutiveUtil(root.right);
+        
+        int inr = 1, dcr = 1;
+        if(root.left!=null){
+            if(root.left.val==root.val+1){
+                inr = left[0]+1;
+            }else if(root.left.val==root.val-1){
+                dcr = left[1]+1;
+            }
+        }
+        
+        if(root.right!=null){
+            if(root.right.val==root.val+1){
+                inr = Math.max(inr, right[0]+1);
+            }else if(root.right.val==root.val-1){
+                dcr = Math.max(dcr, right[1]+1);
+            }
+        }
+        
+        longestPath = Math.max(longestPath, inr+dcr-1);
+        return new int[]{inr, dcr};
+    }
+}
