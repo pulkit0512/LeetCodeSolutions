a7f8ffc41723784adeb93eb468432283a00de7bc
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-29 23:26:13 +0530

    Time: 8 ms (26.82%), Space: 56.6 MB (73.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -15,15 +15,15 @@ class Solution {
     }
     private void bfs(int row, int col, char[][] grid){
         Queue<Integer> que = new LinkedList<>();
-        que.add(row*grid[0].length + col);
+        que.add(row*grid[0].length+col);
         grid[row][col] = '0';
         while(!que.isEmpty()){
-            int val = que.poll();
-            int r = val/grid[0].length;
-            int c = val%grid[0].length;
+            int cur = que.poll();
+            int r = cur/grid[0].length;
+            int c = cur%grid[0].length;
             for(int i=0;i<4;i++){
-                int newRow = r+dir[i][0];
-                int newCol = c+dir[i][1];
+                int newRow = r + dir[i][0];
+                int newCol = c + dir[i][1];
                 if(isValid(newRow, newCol, grid)){
                     que.add(newRow*grid[0].length + newCol);
                     grid[newRow][newCol] = '0';
