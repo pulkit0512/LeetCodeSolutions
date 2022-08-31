97ad9d5c2ca839994e1f391845bd518a455678e0
Pulkit Arora <arorapulkit2@gmail.com>
2022-08-31 21:50:12 +0530

    Time: 21 ms (27.40%), Space: 54 MB (83.05%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,18 +1,21 @@
 class Solution {
-    int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    int[][] dir = new int[][]{{-1,0},{0,-1},{1,0},{0,1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> points = new ArrayList<>();
         int m = heights.length;
         int n = heights[0].length;
-        int water[][] = new int[m][n];
-        Queue<Integer> que = new LinkedList<>();
+        boolean[][] pacificReachable = new boolean[m][n];
+        boolean[][] atlanticReachable = new boolean[m][n];
         
+        Queue<Integer> que = new LinkedList<>();
         for(int i=0;i<n;i++){
             que.add(0*n + i);
         }
         for(int i=1;i<m;i++){
             que.add(i*n + 0);
         }
-        bfs(heights, water, que);
+        bfs(heights, pacificReachable, que);
+        
         
         for(int i=0;i<n;i++){
             que.add((m-1)*n + i);
@@ -20,37 +23,36 @@ class Solution {
         for(int i=0;i<m-1;i++){
             que.add(i*n + n-1);
         }
-        bfs(heights, water, que);
+        bfs(heights, atlanticReachable, que);
         
-        List<List<Integer>> result = new ArrayList<>();
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(water[i][j]==2){
-                    result.add(List.of(i, j));
+                if(pacificReachable[i][j] && atlanticReachable[i][j]){
+                    points.add(List.of(i, j));
                 }
             }
         }
-        return result;
+        
+        return points;
     }
     
-    private void bfs(int[][] heights, int[][] water, Queue<Integer> que) {
+    private void bfs(int[][] heights, boolean[][] reachable, Queue<Integer> que) {
         int m = heights.length;
         int n = heights[0].length;
-        boolean vis[][] = new boolean[m][n];
-        while(!que.isEmpty()) {
+        while(!que.isEmpty()){
             int cur = que.poll();
             int r = cur/n;
             int c = cur%n;
-            if(vis[r][c]){
+            if(reachable[r][c]){
                 continue;
             }
-            water[r][c]++;
-            vis[r][c] = true;
+            reachable[r][c] = true;
             
-            for(int i=0;i<4;i++){
-                int newRow = r + dir[i][0];
-                int newCol = c + dir[i][1];
-                if(newRow<0 || newCol<0 || newRow>=m || newCol>=n || vis[newRow][newCol]){
+            for(int d[]:dir){
+                int newRow = r + d[0];
+                int newCol = c + d[1];
+                if(newRow<0 || newCol<0 || newRow>=m || newCol>=n || reachable[newRow][newCol]){
                     continue;
                 }
                 
