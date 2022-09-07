21da2078df71e3530302d135d7163540bc39c91f
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-07 06:27:38 +0530

    Time: 2 ms (98.77%), Space: 45 MB (73.24%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        tree2strUtil(root, sb);
+        return sb.toString();
+    }
+    
+    private void tree2strUtil(TreeNode root, StringBuilder sb) {
+        if(root == null){
+            return;
+        }
+        
+        sb.append(root.val);
+        
+        // For left child
+        if(root.left!=null){
+            sb.append('(');
+            tree2strUtil(root.left, sb);
+            sb.append(')');
+        }else if(root.left==null && root.right!=null){
+            sb.append("()");
+        }
+        
+        if(root.right!=null){
+            sb.append('(');
+            tree2strUtil(root.right, sb);
+            sb.append(')');
+        }
+    }
+}
