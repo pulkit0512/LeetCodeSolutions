9f8154e8e2e6f0c9ef0b2c964b467ce55382c16e
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-13 00:01:01 +0530

    Time: 14 ms (22.92%), Space: 45.1 MB (19.25%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -5,25 +5,26 @@ class Solution {
     }
     
     private int maxProfitKSpace(int k, int[] prices) {
-        int[] buy = new int[k+1];
-        int[] sell = new int[k+1];
-        Arrays.fill(buy, Integer.MAX_VALUE);
+        int[] cost = new int[k+1];
+        int[] profit = new int[k+1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
         
         for(int i=0;i<prices.length;i++){
             for(int j=1;j<=k;j++){
-                buy[j] = Math.min(buy[j], prices[i]-sell[j-1]);
-                sell[j] = Math.max(sell[j], prices[i]-buy[j]);
+                cost[j] = Math.min(cost[j], prices[i]-profit[j-1]);
+                profit[j] = Math.max(profit[j], prices[i]-cost[j]);
             }
         }
         
-        return sell[k];
+        return profit[k];
     }
     
     private int maxProfitNKSpace(int k, int[] prices) {
         int n = prices.length;
         //3rd dimension to store weather we are holding a stock or not
         // 0 indicates not holding, 1 indicates holding;
-        int[][][] dp= new int[n+1][k+1][2];
+        int dp[][][] = new int[n+1][k+1][2];
+        
         for(int i=n-1;i>=0;i--){
             for(int j=1;j<=k;j++){
                 for(int hold=0;hold<2;hold++){
@@ -34,10 +35,12 @@ class Solution {
                     }else{
                         doSomething = -prices[i] + dp[i+1][j][1];
                     }
+                    
                     dp[i][j][hold] = Math.max(doNothing, doSomething);
                 }
             }
         }
+        
         return dp[0][k][0];
     }
 }
