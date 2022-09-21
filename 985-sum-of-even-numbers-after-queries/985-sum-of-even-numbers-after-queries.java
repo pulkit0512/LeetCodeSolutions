8017c76ba83ae158a68df7301daed5cf754791ad
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-22 00:23:49 +0530

    Time: 5 ms (91.32%), Space: 50.3 MB (95.50%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -10,19 +10,18 @@ class Solution {
             }
         }
         
-        int idx = 0;
-        for(int[] query:queries) {
-            if(nums[query[1]]%2==0){
-                sum -= nums[query[1]];
+        for(int i=0;i<n;i++) {
+            if(nums[queries[i][1]]%2==0){
+                sum -= nums[queries[i][1]];
             }
             
-            nums[query[1]] += query[0];
+            nums[queries[i][1]] += queries[i][0];
             
-            if(nums[query[1]]%2==0){
-                sum += nums[query[1]];
+            if(nums[queries[i][1]]%2==0){
+                sum += nums[queries[i][1]];
             }
             
-            ans[idx++] = sum;
+            ans[i] = sum;
         }
         
         return ans;
