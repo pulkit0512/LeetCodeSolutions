1b9bf88b4658914aed1d0b062d1607967b9faf4e
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-24 00:43:43 +0530

    Time: 177 ms (68.83%), Space: 41.3 MB (59.74%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -2,16 +2,21 @@ class Solution {
     public int concatenatedBinary(int n) {
         int mod = 1000000007;
         
-        int ans = 0;
+        long ans = 0;
+        int length = 0;
         for(int i=1;i<=n;i++){
-            int x = i;
+            /*int x = i;
             while(x>0){
                 x = x/2;
                 ans = (ans<<1)%mod;
+            }*/
+            if((i&(i-1))==0){
+                length++;
             }
+            ans = (((ans<<length)%mod) + i)%mod;
             
-            ans = (ans + i)%mod;
+            //ans = (ans + i)%mod;
         }
-        return ans;
+        return (int)ans;
     }
 }
