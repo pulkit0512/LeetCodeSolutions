769e7b21de94e47034c9ac8c66d97ef7b91d8bb7
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-24 00:47:48 +0530

    Time: 207 ms (66.23%), Space: 40.8 MB (81.82%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -1,21 +1,41 @@
 class Solution {
     public int concatenatedBinary(int n) {
+        
+        //return concatenatedBinaryNLogN(n);
+        return concatenatedBinaryN(n);
+    }
+    
+    private int concatenatedBinaryNLogN(int n) {
+        int mod = 1000000007;
+        
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            // LogN way to get length of current i.
+            int x = i;
+            while(x>0){
+                x = x/2;
+                ans = (ans<<1)%mod;
+            }
+            
+            ans = (ans + i)%mod;
+            
+        }
+        return (int)ans;
+    }
+    
+    private int concatenatedBinaryN(int n) {
         int mod = 1000000007;
         
         long ans = 0;
         int length = 0;
         for(int i=1;i<=n;i++){
-            /*int x = i;
-            while(x>0){
-                x = x/2;
-                ans = (ans<<1)%mod;
-            }*/
+            // O(1) way to see if length is same as previous or increased.
+            // Length will increase only in case of power of 2.
             if((i&(i-1))==0){
                 length++;
             }
             ans = (((ans<<length)%mod) + i)%mod;
             
-            //ans = (ans + i)%mod;
         }
         return (int)ans;
     }
