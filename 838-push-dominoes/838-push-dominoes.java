46e95d763a870e49f47c5d0804de000ba63df3d8
Pulkit Arora <arorapulkit2@gmail.com>
2022-09-28 00:47:42 +0530

    Time: 15 ms (97.01%), Space: 43.5 MB (93.50%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -23,19 +23,19 @@ class Solution {
     
     private void handleLRCases(char prevChar, char curChar, int prevIdx, int curIdx, char[] ans) {
         if(prevChar=='L' && curChar=='L'){
-            fillAns(ans, prevIdx+1, curIdx, prevChar);
+            fillAns(ans, prevIdx+1, curIdx-1, prevChar);
         }else if(prevChar=='R' && curChar=='R'){
-            fillAns(ans, prevIdx+1, curIdx, prevChar);
+            fillAns(ans, prevIdx+1, curIdx-1, prevChar);
         }else if(prevChar=='R' && curChar=='L'){
             int cnt = curIdx-prevIdx-1;
             int half = cnt/2;
-            fillAns(ans, prevIdx+1, prevIdx+1+half, prevChar);
-            fillAns(ans, curIdx-half, curIdx, curChar);
+            fillAns(ans, prevIdx+1, prevIdx+half, prevChar);
+            fillAns(ans, curIdx-half, curIdx-1, curChar);
         }
     }
     
     private void fillAns(char[] ans, int l, int r, char ch) {
-        for(int i=l;i<r;i++){
+        for(int i=l;i<=r;i++){
             ans[i] = ch;
         }
     }
