019a653c2e075a0a7be059613b18aac865f77511
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-01 19:35:28 +0530

    Time: 1 ms (98.72%), Space: 43 MB (10.91%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,61 +1,31 @@
 class Solution {
     public int numDecodings(String s) {
-        return numDecodingsNSpace(s);
-        //return numDecodingsConstantSpace(s);
-    }
-    
-    private int numDecodingsConstantSpace(String s) {
         if(s.charAt(0)=='0'){
             return 0;
         }
-        int n = s.length();
-        int prev = 1;
-        int curr = 1;
-        for(int i=1;i<n;i++){
-            int val = s.charAt(i)-'0';
-            int val1 = s.charAt(i-1)-'0';
-            int temp = curr;
-            if(val!=0){
-                val = val1*10 + val;
-                if(val<=26 && val1!=0){
-                    curr = curr + prev;
-                }
-            }else{
-                if(val1==0 || val1>2){
+        
+        int oneBack = 1, twoBack = 1;
+        int len = s.length();
+        for(int i=1;i<len;i++){
+            int cur = 0;
+            if(s.charAt(i)=='0'){
+                if(s.charAt(i-1)=='0'){
                     return 0;
                 }
-                curr = prev;
-            }
-            prev = temp;
-        }
-        return curr;
-    }
-    
-    private int numDecodingsNSpace(String s) {
-        if(s.charAt(0)=='0'){
-            return 0;
-        }
-        int n = s.length();
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=1;i<n;i++){
-            int val = s.charAt(i)-'0';
-            int val1 = s.charAt(i-1)-'0';
-            if(val!=0){
-                val = val1*10 + val;
-                if(val<=26 && val1!=0){
-                    dp[i+1] = dp[i] + dp[i-1];
-                }else{
-                    dp[i+1] = dp[i];
+                
+                if(s.charAt(i-1)<='2'){
+                    cur += twoBack;
                 }
             }else{
-                if(val1==0 || val1>2){
-                    return 0;
+                cur += oneBack;
+                if(s.charAt(i-1)=='1' || (s.charAt(i-1)=='2' && s.charAt(i)<='6')){
+                    cur += twoBack;
                 }
-                dp[i+1] = dp[i-1];
             }
+            twoBack = oneBack;
+            oneBack = cur;
         }
-        return dp[n];
+        
+        return oneBack;
     }
 }
