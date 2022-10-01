9cf940b6c2ccf2362b38c9a322156542a0beb484
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-02 02:30:07 +0530

    Time: 18 ms (28.22%), Space: 67.5 MB (32.75%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,5 +1,10 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        return maximalSquareNSpace(matrix);
+        //return maximalSquareMNSpace(matrix);
+    }
+    
+    private int maximalSquareNSpace(char[][] matrix) {
         int ans = 0;
         int m = matrix.length;
         int n = matrix[0].length;
@@ -20,6 +25,30 @@ class Solution {
                 }
             }
             prev = cur;
+        }
+        
+        return ans*ans;
+    }
+    
+    private int maximalSquareMNSpace(char[][] matrix) {
+        int ans = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int dp[][] = new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1') {
+                    if(i==0||j==0){
+                        dp[i][j] = 1;
+                    }else{
+                        int val = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                        dp[i][j] = val+1;
+                    }
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
         }
         
         return ans*ans;
