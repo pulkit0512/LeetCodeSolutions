7ad2470fdcb1efb8f84318657453720a7e32db8e
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-02 01:09:58 +0530

    Time: 0 ms (100.00%), Space: 41 MB (90.08%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,18 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+        //1. Transpose the matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[i].length;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for(int j=0;j<n/2;j++){
-            for(int i=0;i<n;i++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = temp;
+        
+        //2. Swap columns
+        for(int i=0;i<matrix.length;i++){
+            int st = 0, ed = matrix[i].length-1;
+            while(st<ed){
+                int temp = matrix[i][st];
+                matrix[i][st] = matrix[i][ed];
+                matrix[i][ed] = temp;
+                st++;
+                ed--;
             }
         }
     }
