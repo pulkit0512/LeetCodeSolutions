5c4b1bd32b35439180a0d8b6c43ac202b6ec7f27
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-01 18:10:05 +0530

    Time: 3 ms (84.08%), Space: 60.7 MB (61.15%) - LeetHub

--- 531-lonely-pixel-i/531-lonely-pixel-i.java
+++ 531-lonely-pixel-i/531-lonely-pixel-i.java
@@ -2,32 +2,52 @@ class Solution {
     public int findLonelyPixel(char[][] picture) {
         int m = picture.length;
         int n = picture[0].length;
-        int[] columnMap = new int[n];
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+        
         for(int i=0;i<m;i++){
             int bCnt = 0;
-            List<Integer> col = new ArrayList<>();
             for(int j=0;j<n;j++){
                 if(picture[i][j]=='B'){
                     bCnt++;
-                    col.add(j);
+                    
+                    if(bCnt>1){
+                        break;
+                    }
                 }
             }
             
             if(bCnt==1){
-                columnMap[col.get(0)]++;
-            }else {
-                for(int c:col){
-                    columnMap[c] = 1000;
+                rows[i] = true;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            int bCnt = 0;
+            for(int j=0;j<m;j++){
+                if(picture[j][i]=='B'){
+                    bCnt++;
+                    
+                    if(bCnt>1){
+                        break;
+                    }
                 }
+            }
+            
+            if(bCnt==1){
+                cols[i] = true;
             }
         }
         
         int ans = 0;
-        for(int colCnt:columnMap){
-            if(colCnt==1){
-                ans++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(picture[i][j]=='B' && rows[i] && cols[j]){
+                    ans++;
+                }
             }
         }
+        
         return ans;
     }
 }
