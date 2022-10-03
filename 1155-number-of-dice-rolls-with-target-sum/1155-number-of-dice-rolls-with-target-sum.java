c59cfff5013539acf84d0c03dc68a622072303f1
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-03 21:10:51 +0530

    Time: 46 ms (34.96%), Space: 42 MB (80.09%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -3,15 +3,38 @@ class Solution {
     int n, k, target;
     int mod = 1000000007;
     public int numRollsToTarget(int n, int k, int target) {
+        //return numRollsToTargetMemo(n, k, target);
+        
+        return numRollsToTargetTabulation(n, k, target);
+    }
+    
+    private int numRollsToTargetTabulation(int n, int k, int target) {
+        int dp[][] = new int[n+1][target+1];
+        dp[n][target] = 1;
+        
+        for(int diceIndex=n-1;diceIndex>=0;diceIndex--){
+            for(int curSum=0;curSum<=target;curSum++){
+                int ways = 0;
+                for(int i=1;i<=Math.min(k, target-curSum);i++){
+                    ways = (ways + dp[diceIndex+1][curSum+i])%mod;
+                }
+                dp[diceIndex][curSum] = ways;
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
+    private int numRollsToTargetMemo(int n, int k, int target) {
         memo = new Integer[n+1][target+1];
         this.k = k;
         this.n = n;
         this.target = target;
         
-        return numRollsToTargetMemo(0, 0);
+        return numRollsToTargetMemoUtil(0, 0);
     }
     
-    private int numRollsToTargetMemo(int diceIndex, int curSum) {
+    private int numRollsToTargetMemoUtil(int diceIndex, int curSum) {
         if(diceIndex==n){
             return curSum==target?1:0;
         }
@@ -22,7 +45,7 @@ class Solution {
         
         int ways = 0;
         for(int i=1;i<=Math.min(k, target-curSum);i++){
-            ways = (ways + numRollsToTargetMemo(diceIndex+1, curSum+i))%mod;
+            ways = (ways + numRollsToTargetMemoUtil(diceIndex+1, curSum+i))%mod;
         }
         
         return memo[diceIndex][curSum] = ways;
