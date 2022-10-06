9198ab51c46404be08bff931909ff56d9c024156
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-06 23:24:13 +0530

    Time: 10 ms (87.82%), Space: 54.6 MB (24.73%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,43 +1,51 @@
 class Solution {
     int ans;
     public int longestIncreasingPath(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int path[][] = new int[n][m];
         ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(path[i][j]==0){
-                    DFS(matrix, path, i, j, n, m);
-                }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] path = new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dfs(matrix, path, i, j);
             }
         }
         
         return ans;
     }
-    private int DFS(int[][] mat, int path[][], int i, int j, int n, int m){
+    
+    private int dfs(int[][] matrix, int[][] path, int i, int j) {
         if(path[i][j]!=0){
             return path[i][j];
         }
-        int val = 0;
-        if(i+1<n && mat[i][j]<mat[i+1][j]){
-            int up = DFS(mat, path, i+1, j, n, m);
-            val = Math.max(val, up);
-        }
-        if(i-1>=0 && mat[i][j]<mat[i-1][j]){
-            int down = DFS(mat, path, i-1, j, n, m);
+        
+        int val = 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        if(i+1<m && matrix[i+1][j]>matrix[i][j]){
+            int down = 1 + dfs(matrix, path, i+1, j);
             val = Math.max(val, down);
         }
-        if(j+1<m && mat[i][j]<mat[i][j+1]){
-            int right = DFS(mat, path, i, j+1, n, m);
+        
+        if(j+1<n && matrix[i][j+1]>matrix[i][j]){
+            int right = 1 + dfs(matrix, path, i, j+1);
             val = Math.max(val, right);
         }
-        if(j-1>=0 && mat[i][j]<mat[i][j-1]){
-            int left = DFS(mat, path, i, j-1, n, m);
+        
+        if(i-1>=0 && matrix[i-1][j]>matrix[i][j]){
+            int up = 1 + dfs(matrix, path, i-1, j);
+            val = Math.max(val, up);
+        }
+        
+        if(j-1>=0 && matrix[i][j-1]>matrix[i][j]){
+            int left = 1 + dfs(matrix, path, i, j-1);
             val = Math.max(val, left);
         }
-        path[i][j] = val+1;
-        ans = Math.max(ans, path[i][j]);
+        
+        path[i][j] = val;
+        ans = Math.max(ans, val);
         return path[i][j];
     }
 }
