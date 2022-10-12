2e3daae8b457e71e335476926aa79d9a4c641cc6
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-12 06:33:55 +0530

    Time: 14 ms (25.92%), Space: 53.4 MB (84.27%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        // a+b>c, a+c>b, b+c>a
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=n-1;i>=2;i--){
+            int sum = nums[i] + nums[i-1] + nums[i-2];
+            
+            if(nums[i] == nums[i-1] && nums[i-1] == nums[i-2]){
+                // Equilateral triangle.
+                return sum;
+            }else if((nums[i] == nums[i-1]) || ((nums[i-1] == nums[i-2]) && (nums[i-1] + nums[i-2] > nums[i]))) {
+                // isosceles triangle
+                return sum;
+            }else if(nums[i] + nums[i-1] > nums[i-2] 
+                     && nums[i] + nums[i-2] > nums[i-1] 
+                     && nums[i-1] + nums[i-2] > nums[i]) {
+                // scalene triangle
+                return sum;
+            }
+        }
+        
+        return 0;
+    }
+}
