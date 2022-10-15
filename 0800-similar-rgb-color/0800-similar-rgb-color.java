d63c4bd7e3aad7d91a8d1d45877c8e8c0f5eece0
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-15 06:53:35 +0530

    Time: 1 ms (100.00%), Space: 40.6 MB (90.48%) - LeetHub

--- 0800-similar-rgb-color/0800-similar-rgb-color.java
+++ 0800-similar-rgb-color/0800-similar-rgb-color.java
@@ -1,52 +1,27 @@
 class Solution {
-    public String similarRGB(String color) {
-        //0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
+    // Given string 'colorSection' representing a two-digit 
+    // base 16 number "AB", find out the number "XX" that 
+    // has the highest similarity to "AB".  
+    public String findTarget(String colorSection) {
+        int num = Integer.parseInt(colorSection, 16);
         
-        int ab = 16*(add(color.charAt(1))) + add(color.charAt(2));
-        int cd = 16*(add(color.charAt(3))) + add(color.charAt(4));
-        int ef = 16*(add(color.charAt(5))) + add(color.charAt(6));
+        // Get the rounded value of num to 17.
+        int x = Math.round((float)num / 17);
         
-        
-        int minAB = Integer.MAX_VALUE;
-        int minCD = minAB, minEF = minAB;
-        
-        int newAB = 0, newCD = 0, newEF = 0;
-        
-        for(int i=0;i<16;i++){
-            int val = 17*i;
-            int absAB = Math.abs(ab-val);
-            int absCD = Math.abs(cd-val);
-            int absEF = Math.abs(ef-val);
-            
-            if(absAB<minAB){
-                minAB = absAB;
-                newAB = val;
-            }
-            
-            if(absCD<minCD){
-                minCD = absCD;
-                newCD = val;
-            }
-            
-            if(absEF<minEF){
-                minEF = absEF;
-                newEF = val;
-            }
-        }
-        StringBuilder res = new StringBuilder(7);
-        res.append('#');
-        res.append(String.format("%02x", (0xFF & newAB)));
-        res.append(String.format("%02x", (0xFF & newCD)));
-        res.append(String.format("%02x", (0xFF & newEF)));
-        
-        return res.toString();
+        // Return "XX", the pattern of the highest similarity.
+        return Integer.toHexString(x).repeat(2);
     }
     
-    private int add(char ch) {
-        if(Character.isDigit(ch)){
-            return ch-'0';
-        }else{
-            return 10 + (ch-'a');
+    public String similarRGB(String color) {
+        StringBuilder targetColor = new StringBuilder();
+        targetColor.append("#");
+        
+        // Split input color into three sections, find out the best
+        // fit for each section and attach it to 'targetColor'.
+        for (int i = 1; i < 6; i += 2) {
+            targetColor.append(findTarget(color.substring(i, i + 2)));
         }
+        
+        return targetColor.toString();
     }
 }
