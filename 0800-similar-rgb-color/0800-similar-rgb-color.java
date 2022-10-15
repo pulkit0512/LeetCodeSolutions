1911a95c1b45c5daa5f7def4ba8ce23f71ce33c8
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-15 06:45:50 +0530

    Time: 12 ms (16.67%), Space: 41.1 MB (85.71%) - LeetHub

--- 0800-similar-rgb-color/0800-similar-rgb-color.java
+++ 0800-similar-rgb-color/0800-similar-rgb-color.java
@@ -2,9 +2,10 @@ class Solution {
     public String similarRGB(String color) {
         //0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
         
-        int ab = Integer.valueOf(color.substring(1,3), 16);
-        int cd = Integer.valueOf(color.substring(3,5), 16);
-        int ef = Integer.valueOf(color.substring(5,7), 16);
+        int ab = 16*(add(color.charAt(1))) + add(color.charAt(2));
+        int cd = 16*(add(color.charAt(3))) + add(color.charAt(4));
+        int ef = 16*(add(color.charAt(5))) + add(color.charAt(6));
+        
         
         int minAB = Integer.MAX_VALUE;
         int minCD = minAB, minEF = minAB;
@@ -32,11 +33,20 @@ class Solution {
                 newEF = val;
             }
         }
-        StringBuilder res = new StringBuilder("#");
+        StringBuilder res = new StringBuilder(7);
+        res.append('#');
         res.append(String.format("%02x", (0xFF & newAB)));
         res.append(String.format("%02x", (0xFF & newCD)));
         res.append(String.format("%02x", (0xFF & newEF)));
         
         return res.toString();
+    }
+    
+    private int add(char ch) {
+        if(Character.isDigit(ch)){
+            return ch-'0';
+        }else{
+            return 10 + (ch-'a');
+        }
     }
 }
