5c8b4b2456888563cc7d8a2290e987da0a77f06a
Pulkit Arora <arorapulkit2@gmail.com>
2022-10-31 01:03:49 +0530

    Time: 121 ms (28.03%), Space: 54.4 MB (37.14%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,40 +1,56 @@
 class Solution {
-    int dir[][] = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+    int dir[][] = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
     public int shortestPath(int[][] grid, int k) {
-        Queue<int[]> pq = new LinkedList<>();
-        Set<String> set = new HashSet<>();
+        
         int m = grid.length;
         int n = grid[0].length;
+        
         if(k>=m+n-2){
             return m+n-2;
         }
-        pq.add(new int[]{0,0,0,0});
+        
+        // Will have an integer array of size 4
+        // arr[0] = x coordinate, arr[1] = y coordinate, arr[2] = obstacles removed till now, arr[3] = steps till now.
+        Queue<int[]> que = new LinkedList<>();
+        // x-coordinate_y-coordinate_obstaclesRemoved
+        Set<String> set = new HashSet<>();
+        
+        que.add(new int[]{0, 0, 0, 0});
         set.add("0_0_0");
-        int minSteps = Integer.MAX_VALUE;
-        while(!pq.isEmpty()){
-            int cur[] = pq.poll();
+        
+        while(!que.isEmpty()) {
+            int cur[] = que.poll();
             if(cur[0]==m-1 && cur[1]==n-1){
-                minSteps = cur[3];
-                break;
+                return cur[3];
             }
+            
             for(int i=0;i<4;i++){
                 int newRow = dir[i][0] + cur[0];
                 int newCol = dir[i][1] + cur[1];
-                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
-                    String s0 = new StringBuilder().append(newRow)
-                        .append('_').append(newCol).append('_').append(cur[2]).toString();
-                    String s1 = new StringBuilder().append(newRow)
-                        .append('_').append(newCol).append('_').append(cur[2]+1).toString();
-                    if(grid[newRow][newCol]==0 && !set.contains(s0)){
-                        pq.add(new int[]{newRow, newCol, cur[2], cur[3]+1});
-                        set.add(s0);
-                    }else if(grid[newRow][newCol]==1 && cur[2]+1<=k && !set.contains(s1)){
-                        pq.add(new int[]{newRow, newCol, cur[2]+1, cur[3]+1});
-                        set.add(s1);
+                
+                if(newRow>=0 && newRow<m && newCol>=0 && newCol<n) {
+                    String noObstacle = new StringBuilder()
+                        .append(newRow).append('_')
+                        .append(newCol).append('_')
+                        .append(cur[2]).toString();
+                
+                    String obstacle = new StringBuilder()
+                        .append(newRow).append('_')
+                        .append(newCol).append('_')
+                        .append(cur[2]+1).toString();
+                    
+                    if(grid[newRow][newCol]==0 && !set.contains(noObstacle)){
+                        que.add(new int[]{newRow, newCol, cur[2], cur[3]+1});
+                        set.add(noObstacle);
+                    }else if(grid[newRow][newCol]==1 && cur[2]+1<=k && !set.contains(obstacle)){
+                        que.add(new int[]{newRow, newCol, cur[2]+1, cur[3]+1});
+                        set.add(obstacle);
                     }
                 }
+                
             }
         }
-        return minSteps==Integer.MAX_VALUE?-1:minSteps;
+        
+        return -1;
     }
 }
